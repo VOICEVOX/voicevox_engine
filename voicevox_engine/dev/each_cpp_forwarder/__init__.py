e72c695259b1305082222fd987cea93c996bda83
buckw6eat <mistlain@gmail.com>
2021-08-21 20:48:32 +0900

    refactor: linter/formatter の指摘を反映

--- voicevox_engine/dev/each_cpp_forwarder/__init__.py
+++ voicevox_engine/dev/each_cpp_forwarder/__init__.py
@@ -1,1 +1,3 @@
-from .mock import *
+from .mock import decode_forward, initialize, yukarin_s_forward, yukarin_sa_forward
+
+__all__ = ["decode_forward", "initialize", "yukarin_s_forward", "yukarin_sa_forward"]
