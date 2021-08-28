128054cf81eb3cb748949a7e3b601d33267ebc8e
buckw6eat <mistlain@gmail.com>
2021-08-29 05:42:05 +0900

    fix: avoid python error with mock
    Now, they will say apology message instead.

--- voicevox_engine/dev/each_cpp_forwarder/mock.py
+++ voicevox_engine/dev/each_cpp_forwarder/mock.py
@@ -1,14 +1,32 @@
-def initialize(*args):
+from logging import getLogger
+from typing import Any, Dict, List
+
+import numpy as np
+
+
+def initialize(*args: List[Any]) -> None:
     pass
 
 
-def yukarin_s_forward(*args):
-    pass
+def yukarin_s_forward(length: int, **kwargs: Dict[str, Any]) -> np.ndarray:
+    logger = getLogger("uvicorn")  # FastAPI / Uvicorn 内からの利用のため
+    logger.info(
+        "Sorry, yukarin_s_forward() is a mock. Return values are incorrect.",
+    )
+    return np.ones(length) / 5
 
 
-def yukarin_sa_forward(*args):
-    pass
+def yukarin_sa_forward(length: int, **kwargs: Dict[str, Any]) -> np.ndarray:
+    logger = getLogger("uvicorn")  # FastAPI / Uvicorn 内からの利用のため
+    logger.info(
+        "Sorry, yukarin_sa_forward() is a mock. Return values are incorrect.",
+    )
+    return np.ones((1, length)) * 5
 
 
-def decode_forward(*args):
-    pass
+def decode_forward(length: int, **kwargs: Dict[str, Any]) -> np.ndarray:
+    logger = getLogger("uvicorn")  # FastAPI / Uvicorn 内からの利用のため
+    logger.info(
+        "Sorry, decode_forward() is a mock. Return values are incorrect.",
+    )
+    return np.ones(length * 256)
