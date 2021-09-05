a4dd7cea89058292793d2c906f091d50e4ed9293
Yosshi999 <Yosshi999@users.noreply.github.com>
2021-09-05 17:19:23 +0900

    remove the confusing text2mora
    cf. https://github.com/Hiroshiba/voicevox_engine/pull/63#discussion_r702276287

--- test/test_mora_list.py
+++ test/test_mora_list.py
@@ -1,9 +1,6 @@
 from unittest import TestCase
 
-from voicevox_engine.mora_list import (
-    openjtalk_mora2text,
-    openjtalk_text2mora
-)
+from voicevox_engine.mora_list import openjtalk_mora2text
 
 class TestOpenJTalkMoraList(TestCase):
     def test_mora2text(self):
@@ -15,20 +12,8 @@ class TestOpenJTalkMoraList(TestCase):
         self.assertEqual("ギェ", openjtalk_mora2text["gye"])
         self.assertEqual("イェ", openjtalk_mora2text["ye"])
 
-    def test_text2mora(self):
-        # GitHub issue #62
-        self.assertEqual("shi", openjtalk_text2mora["シ"])
-        self.assertEqual("si", openjtalk_text2mora["スィ"])
-        self.assertEqual("zi", openjtalk_text2mora["ズィ"])
-        self.assertEqual("ji", openjtalk_text2mora["ジ"])
-
-    def test_bijection(self):
-        # mora -> text -> mora
-        for mora, text in openjtalk_mora2text.items():
-            self.assertIn(text, openjtalk_text2mora)
-            self.assertEqual(mora, openjtalk_text2mora[text])
-
-        # text -> mora -> text
-        for text, mora in openjtalk_text2mora.items():
-            self.assertIn(mora, openjtalk_mora2text)
-            self.assertEqual(text, openjtalk_mora2text[mora])
+    def test_mora2text_injective(self):
+        """異なるモーラが同じ読みがなに対応しないか確認する"""
+        values = list(openjtalk_mora2text.values())
+        uniq_values = list(set(values))
+        self.assertCountEqual(values, uniq_values)
