46b88d098128a08a6cbb25622b47b978442f83fc
Yosshi999 <Yosshi999@users.noreply.github.com>
2021-09-07 23:20:14 +0900

    bugfix: mora_to_textを無声化モーラに対してもうまく動作させる (#70)
    * bugfix: mora_to_text for unvoice moras was broken
    
    Fix a bug in e283fbc
    
    * add tests for run.py
    
    * apply pysen
    
    * remove test due to github action failure

--- run.py
+++ run.py
@@ -74,6 +74,9 @@ def make_synthesis_engine(
 
 
 def mora_to_text(mora: str):
+    if mora[-1:] in ["A", "I", "U", "E", "O"]:
+        # 無声化母音を小文字に
+        mora = mora[:-1] + mora[-1].lower()
     if mora in openjtalk_mora2text:
         return openjtalk_mora2text[mora]
     else:
