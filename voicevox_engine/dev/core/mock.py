46a0d08cfcc05340e15375c16a51499cc0ec3c3d
ISHIDA Naoto <naoto@isnot.jp>
2021-09-09 14:36:48 +0900

    [Mock] /synthesis で、雑音（無音？）の代わりに、固定のダミー・テキストを読み上げる #27 (#77)
    * update: use dummy text instead of void audio for synthesis #27
    
    * fix: add comment #27

--- voicevox_engine/dev/core/mock.py
+++ voicevox_engine/dev/core/mock.py
@@ -2,9 +2,13 @@ from logging import getLogger
 from typing import Any, Dict, List
 
 import numpy as np
+from pyopenjtalk import tts
+from resampy import resample
+
+DUMMY_TEXT = "これはダミーのテキストです"
 
 
-def initialize(*args: List[Any]) -> None:
+def initialize(path: str, use_gpu: bool, *args: List[Any]) -> None:
     pass
 
 
@@ -25,8 +29,42 @@ def yukarin_sa_forward(length: int, **kwargs: Dict[str, Any]) -> np.ndarray:
 
 
 def decode_forward(length: int, **kwargs: Dict[str, Any]) -> np.ndarray:
+    """
+    合成音声の波形データをNumPy配列で返します。ただし、常に固定の文言を読み上げます（DUMMY_TEXT）
+    参照→SynthesisEngine のdocstring [Mock]
+
+    Parameters
+    ----------
+    length : int
+        フレームの長さ
+
+    Returns
+    -------
+    wave : np.ndarray
+        音声合成した波形データ
+
+    Note
+    -------
+        ここで行う音声合成では、調声（ピッチ等）を反映しない
+        また、入力内容によらず常に固定の文言を読み上げる
+
+        # pyopenjtalk.tts()の出力仕様
+        dtype=np.float64, 16 bit, mono 48000 Hz
+
+        # resampleの説明
+        本来はfloat64の入力でも問題ないのかと思われたが、実際には出力が音割れひどかった。
+        対策として、あらかじめint16に型変換しておくと、期待通りの結果になった。
+        非モックdecode_forwardと合わせるために、出力を24kHzに変換した。
+    """
     logger = getLogger("uvicorn")  # FastAPI / Uvicorn 内からの利用のため
     logger.info(
         "Sorry, decode_forward() is a mock. Return values are incorrect.",
     )
-    return np.ones(length * 256)
+    wave, sr = tts(DUMMY_TEXT)
+    wave = resample(
+        wave.astype("int16"),
+        sr,
+        24000,
+        filter="kaiser_fast",
+    )
+    return wave
