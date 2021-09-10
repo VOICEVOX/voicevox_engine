264aff64cfdf4cf91f51972685a080df0f1271c9
buckw6eat <mistlain@gmail.com>
2021-09-11 01:09:31 +0900

    音素化されないテキスト入力に対応 (#82)
    * fix: make sure `utterance` isn't empty
    
    * Apply suggestions from code review
    
    テスト
    
    Co-authored-by: Hiroshiba <hihokaruta@gmail.com>

--- run.py
+++ run.py
@@ -130,6 +130,9 @@ def generate_app(engine: SynthesisEngine) -> FastAPI:
             return []
 
         utterance = extract_full_context_label(text)
+        if len(utterance.breath_groups) == 0:
+            return []
+
         return replace_mora_data(
             accent_phrases=[
                 AccentPhrase(
