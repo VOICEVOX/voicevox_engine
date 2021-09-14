039e033fa7a3f9a92758900f9f57f28935769036
Yosshi999 <Yosshi999@users.noreply.github.com>
2021-09-15 00:04:33 +0900

    run.pyのテスト追加 (#72)
    * bugfix: mora_to_text for unvoice moras was broken
    
    Fix a bug in e283fbc
    
    * add tests for run.py
    
    * apply pysen
    
    * add invalid case
    
    * add sndfile for ubuntu

---
+++ test/test_run.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+
+from run import mora_to_text
+
+
+class TestMoraToText(TestCase):
+    def test_voice(self):
+        self.assertEqual(mora_to_text("a"), "ア")
+        self.assertEqual(mora_to_text("i"), "イ")
+        self.assertEqual(mora_to_text("ka"), "カ")
+        self.assertEqual(mora_to_text("N"), "ン")
+        self.assertEqual(mora_to_text("cl"), "ッ")
+        self.assertEqual(mora_to_text("gye"), "ギェ")
+        self.assertEqual(mora_to_text("ye"), "イェ")
+        self.assertEqual(mora_to_text("wo"), "ウォ")
+
+    def test_unvoice(self):
+        self.assertEqual(mora_to_text("A"), "ア")
+        self.assertEqual(mora_to_text("I"), "イ")
+        self.assertEqual(mora_to_text("kA"), "カ")
+        self.assertEqual(mora_to_text("gyE"), "ギェ")
+        self.assertEqual(mora_to_text("yE"), "イェ")
+        self.assertEqual(mora_to_text("wO"), "ウォ")
+
+    def test_invalid_mora(self):
+        """変なモーラが来ても例外を投げない"""
+        self.assertEqual(mora_to_text("x"), "x")
+        self.assertEqual(mora_to_text(""), "")
