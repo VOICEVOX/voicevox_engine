3d3b139c0c2de75739570ccd72cb6800c62f3638
y-chan <y-chan@y-chan.dev>
2021-10-30 17:30:54 +0900

    [COMPLETE] add synthesis engine test

--- test/test_synthesis_engine.py
+++ test/test_synthesis_engine.py
@@ -11,6 +11,7 @@ from voicevox_engine.acoustic_feature_extractor import OjtPhoneme
 from voicevox_engine.model import AccentPhrase, AudioQuery, Mora
 from voicevox_engine.synthesis_engine import (
     SynthesisEngine,
+    pre_process,
     split_mora,
     to_flatten_moras,
     to_phoneme_data_list,
@@ -244,6 +245,44 @@ class TestSynthesisEngine(TestCase):
                 OjtPhoneme(phoneme="s", start=17, end=18),
                 None,
             ],
+        )
+
+    def test_pre_process(self):
+        flatten_moras, phoneme_data_list = pre_process(
+            deepcopy(self.accent_phrases_hello_hiho)
+        )
+
+        mora_index = 0
+        phoneme_index = 1
+
+        self.assertEqual(phoneme_data_list[0], OjtPhoneme("pau", 0, 1))
+        for accent_phrase in self.accent_phrases_hello_hiho:
+            moras = accent_phrase.moras
+            for mora in moras:
+                self.assertEqual(flatten_moras[mora_index], mora)
+                mora_index += 1
+                if mora.consonant is not None:
+                    self.assertEqual(
+                        phoneme_data_list[phoneme_index],
+                        OjtPhoneme(mora.consonant, phoneme_index, phoneme_index + 1),
+                    )
+                    phoneme_index += 1
+                self.assertEqual(
+                    phoneme_data_list[phoneme_index],
+                    OjtPhoneme(mora.vowel, phoneme_index, phoneme_index + 1),
+                )
+                phoneme_index += 1
+            if accent_phrase.pause_mora:
+                self.assertEqual(flatten_moras[mora_index], accent_phrase.pause_mora)
+                mora_index += 1
+                self.assertEqual(
+                    phoneme_data_list[phoneme_index],
+                    OjtPhoneme("pau", phoneme_index, phoneme_index + 1),
+                )
+                phoneme_index += 1
+        self.assertEqual(
+            phoneme_data_list[phoneme_index],
+            OjtPhoneme("pau", phoneme_index, phoneme_index + 1),
         )
 
     def test_replace_phoneme_length(self):
