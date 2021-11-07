4c0746b13ca725f86e460cf23e10506ff3e3624f
takana-v <44311840+takana-v@users.noreply.github.com>
2021-11-07 16:25:44 +0900

    fix mock

--- voicevox_engine/dev/core/mock.py
+++ voicevox_engine/dev/core/mock.py
@@ -78,13 +78,23 @@ def metas() -> str:
         [
             {
                 "name": "dummy1",
-                "styles": [{"name": "dummy1", "id": 0}],
+                "styles": [
+                    {"name": "dummy1", "id": 0},
+                    {"name": "dummy1(style1)", "id": 2},
+                    {"name": "dummy1(style2)", "id": 4},
+                    {"name": "dummy1(style3)", "id": 6},
+                ],
                 "speaker_uuid": "7ffcb7ce-00ec-4bdc-82cd-45a8889e43ff",
                 "version": version,
             },
             {
                 "name": "dummy2",
-                "styles": [{"name": "dummy2", "id": 1}],
+                "styles": [
+                    {"name": "dummy2", "id": 1},
+                    {"name": "dummy2(style1)", "id": 3},
+                    {"name": "dummy2(style2)", "id": 5},
+                    {"name": "dummy2(style3)", "id": 7},
+                ],
                 "speaker_uuid": "388f246b-8c41-4ac1-8e2d-5d79f3ff56d9",
                 "version": version,
             },
