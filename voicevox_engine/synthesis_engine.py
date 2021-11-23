3b3c3ae308cd4fa271e252380867ef3c041f09ca
aoirint <aoirint@gmail.com>
2021-11-23 19:01:10 +0900

    mora_to_text: return value typehint

--- voicevox_engine/synthesis_engine.py
+++ voicevox_engine/synthesis_engine.py
@@ -127,7 +127,7 @@ def pre_process(
     return flatten_moras, phoneme_data_list
 
 
-def mora_to_text(mora: str):
+def mora_to_text(mora: str) -> str:
     if mora[-1:] in ["A", "I", "U", "E", "O"]:
         # 無声化母音を小文字に
         mora = mora[:-1] + mora[-1].lower()
