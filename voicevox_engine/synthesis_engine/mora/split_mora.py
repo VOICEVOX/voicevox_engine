b7bd23ab70c4025ec49d4c35da1c7f469efa0170
aoirint <aoirint@gmail.com>
2021-12-03 07:53:41 +0900

    use generics with bound type

--- voicevox_engine/synthesis_engine/mora/split_mora.py
+++ voicevox_engine/synthesis_engine/mora/split_mora.py
@@ -1,27 +1,29 @@
-from typing import List, Optional
+from typing import List, Optional, TypeVar
 
-from ...acoustic_feature_extractor import OjtPhoneme
+from ...acoustic_feature_extractor import BasePhoneme
 
 unvoiced_mora_phoneme_list = ["A", "I", "U", "E", "O", "cl", "pau"]
 mora_phoneme_list = ["a", "i", "u", "e", "o", "N"] + unvoiced_mora_phoneme_list
 
+P = TypeVar("P", bound=BasePhoneme)
 
-def split_mora(phoneme_list: List[OjtPhoneme]):
+
+def split_mora(phoneme_list: List[P]):
     """
-    OjtPhonemeのリストから、
+    BasePhonemeを継承するPのリストから、
     母音の位置(vowel_indexes)
     母音の音素列(vowel_phoneme_list)
     子音の音素列(consonant_phoneme_list)
     を生成し、返す
     Parameters
     ----------
-    phoneme_list : List[OjtPhoneme]
+    phoneme_list : List[P]
         phonemeクラスのリスト
     Returns
     -------
-    consonant_phoneme_list : List[OjtPhoneme]
+    consonant_phoneme_list : List[P]
         子音の音素列
-    vowel_phoneme_list : List[OjtPhoneme]
+    vowel_phoneme_list : List[P]
         母音の音素列
     vowel_indexes : : List[int]
         母音の位置
@@ -35,7 +37,7 @@ def split_mora(phoneme_list: List[OjtPhoneme]):
     # 1の場合はconsonant(子音)が存在しない=母音のみ(a/i/u/e/o/N/cl/pau)で構成されるモーラ(音)である
     # 2の場合はconsonantが存在するモーラである
     # なので、2の場合(else)でphonemeを取り出している
-    consonant_phoneme_list: List[Optional[OjtPhoneme]] = [None] + [
+    consonant_phoneme_list: List[Optional[P]] = [None] + [
         None if post - prev == 1 else phoneme_list[post - 1]
         for prev, post in zip(vowel_indexes[:-1], vowel_indexes[1:])
     ]
