f5d5b1756065c97b3b33b192eb7ce19acb6ff02d
aoirint <aoirint@gmail.com>
2021-12-06 17:27:36 +0900

    add docstring

--- voicevox_engine/morphing.py
+++ voicevox_engine/morphing.py
@@ -77,8 +77,21 @@ def synthesis_world(
     output_stereo: bool = False,
 ) -> np.ndarray:
     """
-    指定された2人の話者で音声を合成、指定した割合でモーフィングした音声を得ます。
-    モーフィングの割合は`morph_rate`で指定でき、0.0でベースの話者、1.0でターゲットの話者に近づきます。
+    指定した割合で、パラメータをもとにモーフィングした音声を生成します。
+
+    Parameters
+    ----------
+    morh_param : WorldParameters
+        `synthesis_world_parameters`または`create_world_parameters`で作成したパラメータ
+
+    morh_rate : float
+        モーフィングの割合 [0.0, 1.0]
+        0.0でベースの話者、1.0でターゲットの話者に近づきます。
+
+    Returns
+    -------
+    generated : np.ndarray
+        モーフィングされた音声
     """
 
     if morph_rate < 0.0 or morph_rate > 1.0:
