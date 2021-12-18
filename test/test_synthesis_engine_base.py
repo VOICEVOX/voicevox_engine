dc76e253184a38318a30fff65df3cde3d89e0221
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2021-12-18 15:18:30 +0900

    fixed: MockSynthesisEngineを使用するように変更した
    このテストケースではSynthesisEngineを使用する必要がないため

--- test/test_synthesis_engine_base.py
+++ test/test_synthesis_engine_base.py
@@ -1,84 +1,14 @@
-from typing import List, Union
+from typing import List
 from unittest import TestCase
-from unittest.mock import Mock
 
-import numpy
-
+from voicevox_engine.dev.synthesis_engine.mock import MockSynthesisEngine
 from voicevox_engine.model import AccentPhrase, Mora
-from voicevox_engine.synthesis_engine import SynthesisEngine
-
-
-def yukarin_s_mock(length: int, phoneme_list: numpy.ndarray, speaker_id: numpy.ndarray):
-    result = []
-    # mockとしての適当な処理、特に意味はない
-    for i in range(length):
-        result.append(float(phoneme_list[i] * 0.5 + speaker_id))
-    return numpy.array(result)
-
-
-def yukarin_sa_mock(
-    length: int,
-    vowel_phoneme_list: numpy.ndarray,
-    consonant_phoneme_list: numpy.ndarray,
-    start_accent_list: numpy.ndarray,
-    end_accent_list: numpy.ndarray,
-    start_accent_phrase_list: numpy.ndarray,
-    end_accent_phrase_list: numpy.ndarray,
-    speaker_id: numpy.ndarray,
-):
-    result = []
-    # mockとしての適当な処理、特に意味はない
-    for i in range(length):
-        result.append(
-            float(
-                (
-                    vowel_phoneme_list[0][i]
-                    + consonant_phoneme_list[0][i]
-                    + start_accent_list[0][i]
-                    + end_accent_list[0][i]
-                    + start_accent_phrase_list[0][i]
-                    + end_accent_phrase_list[0][i]
-                )
-                * 0.5
-                + speaker_id
-            )
-        )
-    return numpy.array(result)[numpy.newaxis]
-
-
-def decode_mock(
-    length: int,
-    phoneme_size: int,
-    f0: numpy.ndarray,
-    phoneme: numpy.ndarray,
-    speaker_id: Union[numpy.ndarray, int],
-):
-    result = []
-    # mockとしての適当な処理、特に意味はない
-    for i in range(length):
-        # decode forwardはデータサイズがlengthの256倍になるのでとりあえず256回データをresultに入れる
-        for _ in range(256):
-            result.append(
-                float(
-                    f0[i][0] * (numpy.where(phoneme[i] == 1)[0] / phoneme_size)
-                    + speaker_id
-                )
-            )
-    return numpy.array(result)
 
 
 class TestSynthesisEngineBase(TestCase):
     def setUp(self):
         super().setUp()
-        self.yukarin_s_mock = Mock(side_effect=yukarin_s_mock)
-        self.yukarin_sa_mock = Mock(side_effect=yukarin_sa_mock)
-        self.decode_mock = Mock(side_effect=decode_mock)
-        self.synthesis_engine = SynthesisEngine(
-            yukarin_s_forwarder=self.yukarin_s_mock,
-            yukarin_sa_forwarder=self.yukarin_sa_mock,
-            decode_forwarder=self.decode_mock,
-            speakers="",
-        )
+        self.synthesis_engine = MockSynthesisEngine(speakers="")
 
     def create_accent_phrases_test_base(
         self, text: str, expected: List[AccentPhrase], enable_interrogative: bool
@@ -104,26 +34,26 @@ class TestSynthesisEngineBase(TestCase):
                         Mora(
                             text="コ",
                             consonant="k",
-                            consonant_length=12.5,
+                            consonant_length=0,
                             vowel="o",
-                            vowel_length=16.0,
-                            pitch=28.0,
+                            vowel_length=0,
+                            pitch=0,
                         ),
                         Mora(
                             text="レ",
                             consonant="r",
-                            consonant_length=17.5,
+                            consonant_length=0,
                             vowel="e",
-                            vowel_length=8.0,
-                            pitch=25.0,
+                            vowel_length=0,
+                            pitch=0,
                         ),
                         Mora(
                             text="ワ",
                             consonant="w",
-                            consonant_length=22.0,
+                            consonant_length=0,
                             vowel="a",
-                            vowel_length=4.5,
-                            pitch=26.5,
+                            vowel_length=0,
+                            pitch=0,
                         ),
                     ],
                     accent=3,
@@ -136,40 +66,40 @@ class TestSynthesisEngineBase(TestCase):
                             consonant=None,
                             consonant_length=None,
                             vowel="a",
-                            vowel_length=4.5,
-                            pitch=4.5,
+                            vowel_length=0,
+                            pitch=0,
                         ),
                         Mora(
                             text="リ",
                             consonant="r",
-                            consonant_length=17.5,
+                            consonant_length=0,
                             vowel="i",
-                            vowel_length=11.5,
-                            pitch=28.5,
+                            vowel_length=0,
+                            pitch=0,
                         ),
                         Mora(
                             text="マ",
                             consonant="m",
-                            consonant_length=14.0,
+                            consonant_length=0,
                             vowel="a",
-                            vowel_length=4.5,
-                            pitch=18.0,
+                            vowel_length=0,
+                            pitch=0,
                         ),
                         Mora(
                             text="ス",
                             consonant="s",
-                            consonant_length=18.5,
+                            consonant_length=0,
                             vowel="U",
-                            vowel_length=4.0,
-                            pitch=0.0,
+                            vowel_length=0,
+                            pitch=0,
                         ),
                         Mora(
                             text="カ",
                             consonant="k",
-                            consonant_length=12.5,
+                            consonant_length=0,
                             vowel="a",
-                            vowel_length=4.5,
-                            pitch=16.5,
+                            vowel_length=0,
+                            pitch=0,
                         ),
                     ],
                     accent=3,
@@ -178,7 +108,6 @@ class TestSynthesisEngineBase(TestCase):
             ]
 
         expected = koreha_arimasuka_base_expected()
-        expected[-1].moras[-1].pitch = 16.0
         expected[-1].moras += [
             Mora(
                 text="ア",
@@ -186,7 +115,7 @@ class TestSynthesisEngineBase(TestCase):
                 consonant_length=None,
                 vowel="a",
                 vowel_length=0.15,
-                pitch=6.5,
+                pitch=0.3,
             )
         ]
         self.create_accent_phrases_test_base(
@@ -218,8 +147,8 @@ class TestSynthesisEngineBase(TestCase):
                             consonant=None,
                             consonant_length=None,
                             vowel="N",
-                            vowel_length=3.0,
-                            pitch=4.5,
+                            vowel_length=0,
+                            pitch=0,
                         ),
                     ],
                     accent=1,
@@ -235,7 +164,6 @@ class TestSynthesisEngineBase(TestCase):
         )
 
         expected = nn_base_expected()
-        expected[-1].moras[-1].pitch = 4.0
         expected[-1].moras += [
             Mora(
                 text="ン",
@@ -243,7 +171,7 @@ class TestSynthesisEngineBase(TestCase):
                 consonant_length=None,
                 vowel="N",
                 vowel_length=0.15,
-                pitch=4.3,
+                pitch=0.3,
             )
         ]
         self.create_accent_phrases_test_base(
@@ -268,7 +196,7 @@ class TestSynthesisEngineBase(TestCase):
                             consonant=None,
                             consonant_length=None,
                             vowel="cl",
-                            vowel_length=6.5,
+                            vowel_length=0,
                             pitch=0.0,
                         ),
                     ],
@@ -285,7 +213,6 @@ class TestSynthesisEngineBase(TestCase):
         )
 
         expected = ltu_base_expected()
-        expected[-1].moras[-1].pitch = 0.0
         expected[-1].moras += [
             Mora(
                 text="ッ",
@@ -316,10 +243,10 @@ class TestSynthesisEngineBase(TestCase):
                         Mora(
                             text="ス",
                             consonant="s",
-                            consonant_length=18.5,
+                            consonant_length=0,
                             vowel="u",
-                            vowel_length=21.0,
-                            pitch=40.5,
+                            vowel_length=0,
+                            pitch=0,
                         ),
                     ],
                     accent=1,
@@ -335,7 +262,6 @@ class TestSynthesisEngineBase(TestCase):
         )
 
         expected = su_base_expected()
-        expected[-1].moras[-1].pitch = 40.0
         expected[-1].moras += [
             Mora(
                 text="ウ",
@@ -343,7 +269,7 @@ class TestSynthesisEngineBase(TestCase):
                 consonant_length=None,
                 vowel="u",
                 vowel_length=0.15,
-                pitch=6.5,
+                pitch=0.3,
             )
         ]
         self.create_accent_phrases_test_base(
