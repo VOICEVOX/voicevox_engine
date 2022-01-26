2e0497241a46731a570708c93aa33323b61d97d8
takana-v <44311840+takana-v@users.noreply.github.com>
2022-01-27 03:25:00 +0900

    検証の厳密化（アクセント型）

--- voicevox_engine/model.py
+++ voicevox_engine/model.py
@@ -187,7 +187,7 @@ class UserDictWord(BaseModel):
                     values["pronunciation"],
                 )
             )
-        if mora_count < values["accent_type"]:
+        if 0 < mora_count < values["accent_type"]:
             raise ValueError(f"誤ったアクセント型です({values['accent_type']})。 expect: 0 < accent_type < {mora_count}")
         return mora_count
 
