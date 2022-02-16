0c599262e708874030fda31e98fecea62b7f28b8
Hiroshiba Kazuyuki <hihokaruta@gmail.com>
2022-02-17 01:38:53 +0900

    test

--- build_util/test_release_build.py
+++ build_util/test_release_build.py
@@ -2,10 +2,14 @@
 ビルド結果をテストする
 """
 import argparse
+import json
 import time
+from io import BytesIO
 from pathlib import Path
 from subprocess import Popen
 from urllib.request import Request, urlopen
+
+import soundfile
 
 base_url = "http://localhost:50021/"
 
@@ -19,6 +23,22 @@ def test_release_build(dist_dir: Path) -> None:
     req = Request(base_url + "version")
     with urlopen(req) as res:
         assert len(res.read()) > 0
+
+    # テキスト -> クエリ
+    text = "こんにちは、音声合成の世界へようこそ"
+    req = Request(base_url + "audio_query?speaker=1")
+    req.add_header("Content-Type", "application/json")
+    req.data = json.dumps({"text": text}).encode("utf-8")
+    with urlopen(req) as res:
+        query = json.loads(res.read().decode("utf-8"))
+
+    # クエリ -> 音声
+    req = Request(base_url + "synthesis?speaker=1")
+    req.add_header("Content-Type", "application/json")
+    req.data = json.dumps(query).encode("utf-8")
+    with urlopen(req) as res:
+        wave = res.read()
+    soundfile.read(BytesIO(wave))
 
     # プロセスが稼働中であることを確認
     assert process.poll() is None
