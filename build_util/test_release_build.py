0a636b8c1867590736f5e6a7dc382ca0aac5b333
Hiroshiba Kazuyuki <hihokaruta@gmail.com>
2022-02-17 01:56:37 +0900

    a

--- build_util/test_release_build.py
+++ build_util/test_release_build.py
@@ -27,12 +27,15 @@ def test_release_build(dist_dir: Path) -> None:
 
     # テキスト -> クエリ
     text = "こんにちは、音声合成の世界へようこそ"
-    req = Request(base_url + "audio_query?" + urlencode({"speaker": "1", "text": text}))
+    req = Request(
+        base_url + "audio_query?" + urlencode({"speaker": "1", "text": text}),
+        method="POST",
+    )
     with urlopen(req) as res:
         query = json.loads(res.read().decode("utf-8"))
 
     # クエリ -> 音声
-    req = Request(base_url + "synthesis?speaker=1")
+    req = Request(base_url + "synthesis?speaker=1", method="POST")
     req.add_header("Content-Type", "application/json")
     req.data = json.dumps(query).encode("utf-8")
     with urlopen(req) as res:
