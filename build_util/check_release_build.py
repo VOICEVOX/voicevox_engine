5d54c904517211af7fc7f016cee0790a77ed24f9
Hiroshiba Kazuyuki <hihokaruta@gmail.com>
2022-02-17 03:21:58 +0900

    .

--- build_util/check_release_build.py
+++ build_util/check_release_build.py
@@ -7,7 +7,6 @@ import time
 from io import BytesIO
 from pathlib import Path
 from subprocess import Popen
-from typing import Optional
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
@@ -21,52 +20,37 @@ def test_release_build(dist_dir: Path) -> None:
     if not run_file.exists():
         run_file = dist_dir / "run.exe"
 
-    error: Optional[Exception] = None
+    # 起動
+    process = Popen([run_file.absolute()], cwd=dist_dir)
+    time.sleep(10)  # 待機
 
-    try:
-        # 起動
-        process = Popen([run_file.absolute()], cwd=dist_dir)
-        time.sleep(30)  # 待機
+    # バージョン取得テスト
+    req = Request(base_url + "version")
+    with urlopen(req) as res:
+        assert len(res.read()) > 0
 
-        # バージョン取得テスト
-        req = Request(base_url + "version")
-        with urlopen(req) as res:
-            assert len(res.read()) > 0
+    # テキスト -> クエリ
+    text = "こんにちは、音声合成の世界へようこそ"
+    req = Request(
+        base_url + "audio_query?" + urlencode({"speaker": "1", "text": text}),
+        method="POST",
+    )
+    with urlopen(req) as res:
+        query = json.loads(res.read().decode("utf-8"))
 
-        # テキスト -> クエリ
-        text = "こんにちは、音声合成の世界へようこそ"
-        req = Request(
-            base_url + "audio_query?" + urlencode({"speaker": "1", "text": text}),
-            method="POST",
-        )
-        with urlopen(req) as res:
-            query = json.loads(res.read().decode("utf-8"))
+    # クエリ -> 音声
+    req = Request(base_url + "synthesis?speaker=1", method="POST")
+    req.add_header("Content-Type", "application/json")
+    req.data = json.dumps(query).encode("utf-8")
+    with urlopen(req) as res:
+        wave = res.read()
+    soundfile.read(BytesIO(wave))
 
-        # クエリ -> 音声
-        req = Request(base_url + "synthesis?speaker=1", method="POST")
-        req.add_header("Content-Type", "application/json")
-        req.data = json.dumps(query).encode("utf-8")
-        with urlopen(req) as res:
-            wave = res.read()
-        soundfile.read(BytesIO(wave))
-
-        # プロセスが稼働中であることを確認
-        assert process.poll() is None
-
-    except Exception as e:
-        stdout, stderr = process.communicate()
-        print("--engine stdout--")
-        print(stdout.decode("utf-8"))
-        print("--engine stderr--")
-        print(stderr.decode("utf-8"))
-
-        error = e
+    # プロセスが稼働中であることを確認
+    assert process.poll() is None
 
     # 停止
-    process.kill()
-
-    if error:
-        raise error
+    process.terminate()
 
 
 if __name__ == "__main__":
