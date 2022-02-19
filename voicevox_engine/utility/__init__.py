ee750943a25f56b70b459bbd535250b18bc33f50
takana-v <44311840+takana-v@users.noreply.github.com>
2022-02-20 03:39:11 +0900

    engine_root関数を追加

--- voicevox_engine/utility/__init__.py
+++ voicevox_engine/utility/__init__.py
@@ -3,9 +3,11 @@ from .connect_base64_waves import (
     connect_base64_waves,
     decode_base64_waves,
 )
+from .engine_root import engine_root
 
 __all__ = [
     "ConnectBase64WavesException",
     "connect_base64_waves",
     "decode_base64_waves",
+    "engine_root",
 ]
