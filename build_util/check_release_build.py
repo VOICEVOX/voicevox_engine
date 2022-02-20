f238d88a324db4b87915696c72e2f1e943c271ac
Hiroshiba <hihokaruta@gmail.com>
2022-02-20 19:56:54 +0900

    ビルド結果を自動テストする (#329)
    * ビルド結果を自動テストする
    
    * .
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * test
    
    * a
    
    * a
    
    * a
    
    * a
    
    * test
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * show erorr
    
    * a
    
    * a
    
    * abs
    
    * add er
    
    * kill
    
    * more wait
    
    * .
    
    * after build only
    
    * 60秒待機にしてmacも追加してみる
    
    * add workflow_dispatch inputs

---
+++ build_util/check_release_build.py
@@ -0,0 +1,60 @@
+"""
+ビルド結果をテストする
+"""
+import argparse
+import json
+import time
+from io import BytesIO
+from pathlib import Path
+from subprocess import Popen
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+import soundfile
+
+base_url = "http://localhost:50021/"
+
+
+def test_release_build(dist_dir: Path) -> None:
+    run_file = dist_dir / "run"
+    if not run_file.exists():
+        run_file = dist_dir / "run.exe"
+
+    # 起動
+    process = Popen([run_file.absolute()], cwd=dist_dir)
+    time.sleep(60)  # 待機
+
+    # バージョン取得テスト
+    req = Request(base_url + "version")
+    with urlopen(req) as res:
+        assert len(res.read()) > 0
+
+    # テキスト -> クエリ
+    text = "こんにちは、音声合成の世界へようこそ"
+    req = Request(
+        base_url + "audio_query?" + urlencode({"speaker": "1", "text": text}),
+        method="POST",
+    )
+    with urlopen(req) as res:
+        query = json.loads(res.read().decode("utf-8"))
+
+    # クエリ -> 音声
+    req = Request(base_url + "synthesis?speaker=1", method="POST")
+    req.add_header("Content-Type", "application/json")
+    req.data = json.dumps(query).encode("utf-8")
+    with urlopen(req) as res:
+        wave = res.read()
+    soundfile.read(BytesIO(wave))
+
+    # プロセスが稼働中であることを確認
+    assert process.poll() is None
+
+    # 停止
+    process.terminate()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dist_dir", type=Path, default=Path("dist/"))
+    args = parser.parse_args()
+    test_release_build(dist_dir=args.dist_dir)
