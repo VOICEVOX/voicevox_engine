78c846eb73fb684ee77d613b4741fca21bee8e84
Hiroshiba <hihokaruta@gmail.com>
2022-02-28 21:40:15 +0900

    Merge 0.11.2 (#351)
    * To 0.11.0 (#346)
    
    * ドキュメントの更新
    
    * a
    
    * a
    
    * to 0.11.0
    
    * To 0.11.1 (#350)
    
    * release-testのcallのバージョン指定の修正 (#347)
    
    * release-testのcallのバージョン指定の修正
    
    * 時間を伸ばした
    
    * ユーザー辞書についての文をreadmeに追加 (#348)
    
    * ユーザ辞書機能について文章を追加
    
    * 見出しを修正
    
    * 改行を追加
    
    * envの代わりにgithubコンテキストを使うよう変更 (#349)
    
    called workflow では caller workflow で定義された env コンテキストの
    変数が使えないらしい
    cf. https://docs.github.com/ja/actions/using-workflows/reusing-workflows#limitations
    
    * VOICEVOX_CORE_VERSION 0.11.1
    
    Co-authored-by: takana-v <44311840+takana-v@users.noreply.github.com>
    Co-authored-by: Gray Suitcase <41382894+PickledChair@users.noreply.github.com>
    
    Co-authored-by: takana-v <44311840+takana-v@users.noreply.github.com>
    Co-authored-by: Gray Suitcase <41382894+PickledChair@users.noreply.github.com>

--- make_docs.py
+++ make_docs.py
@@ -1,12 +1,15 @@
 import json
 
-import run
-
 from voicevox_engine.dev.core import mock as core
 from voicevox_engine.dev.synthesis_engine.mock import MockSynthesisEngine
 
 if __name__ == "__main__":
-    app = run.generate_app(MockSynthesisEngine(speakers=core.metas()))
+    import run
+
+    app = run.generate_app(
+        synthesis_engines={"mock": MockSynthesisEngine(speakers=core.metas())},
+        latest_core_version="mock",
+    )
     with open("docs/api/index.html", "w") as f:
         f.write(
             """<!DOCTYPE html>
