0b1bfe70524c41994ccee9a71e8e06c892ca001d
y-chan <y-chan@y-chan.dev>
2022-03-13 14:36:34 +0900

    fix model field title

--- voicevox_engine/model.py
+++ voicevox_engine/model.py
@@ -233,4 +233,4 @@ class SupportedDevicesInfo(BaseModel):
 
     cpu: bool = Field(title="CPUに対応しているか")
     cuda: bool = Field(title="CUDA(Nvidia GPU)に対応しているか")
-    dml: bool = Field(title="DirectML(Radeon GPU)に対応しているか")
+    dml: bool = Field(title="DirectML(Nvidia GPU/Radeon GPU等)に対応しているか")
