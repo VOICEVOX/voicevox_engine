23717d8d5a742e5c1fca039aad46e22ee698c266
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-06-26 17:08:12 +0900

    Change: iconをパスに

--- generate_manifest.py
+++ generate_manifest.py
@@ -1,15 +1,12 @@
-import base64
 import json
 import os
 from pathlib import Path
 
 
-def generate_manifest(base: str, icon_path: str) -> dict:
+def generate_manifest(base: str) -> dict:
     with open(base, "r") as f:
         manifest = json.load(f)
     manifest["version"] = os.getenv("VOICEVOX_ENGINE_VERSION")
-    with open(icon_path, "rb") as f:
-        manifest["icon"] = base64.b64encode(f.read()).decode()
     return manifest
 
 
@@ -19,15 +16,13 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--output_path", type=str)
-    parser.add_argument("-i", "--icon_path", type=str)
     parser.add_argument("input", type=str, nargs="?")
     args = parser.parse_args()
 
     output_path = args.output_path
-    icon_path = args.icon_path or "manifest_assets/icon.png"
     input_path = args.input or "manifest_assets/manifest.json"
 
-    manifest = generate_manifest(input_path, icon_path)
+    manifest = generate_manifest(input_path)
 
     # dump
     out = Path(output_path).open("w") if output_path else sys.stdout
