572dfcf02b784eeb42fdbf965c26e496fa8ed236
Hiroshiba <hihokaruta@gmail.com>
2022-07-03 23:39:31 +0900

    製品のマニフェストを取ってくる (#429)
    * stash
    
    * マニフェストをリリースに組み込む
    
    * update resource version
    
    * .
    
    * .
    
    * manifest.jsonをルートに移動
    
    * engine_manifest_assets
    
    * to 0.13.0-preview.2
    
    * 相対パスを正しく
    
    * ビルドにengine_manifest.jsonを追加
    
    * readmeとかを更新

--- voicevox_engine/engine_manifest/EngineManifestLoader.py
+++ voicevox_engine/engine_manifest/EngineManifestLoader.py
@@ -6,28 +6,35 @@ from .EngineManifest import EngineManifest, LicenseInfo, UpdateInfo
 
 
 class EngineManifestLoader:
-    def __init__(self, assets_dir: Path):
-        self.assets_dir = assets_dir
+    def __init__(self, manifest_path: Path, root_dir: Path):
+        self.manifest_path = manifest_path
+        self.root_dir = root_dir
 
     def load_manifest(self) -> EngineManifest:
+        manifest = json.loads(self.manifest_path.read_text(encoding="utf-8"))
+
         manifest = EngineManifest(
-            **json.load((self.assets_dir / "manifest.json").open(encoding="utf-8")),
-            icon=b64encode((self.assets_dir / "icon.png").read_bytes()).decode("utf-8"),
-            terms_of_service=(self.assets_dir / "terms_of_service.md").read_text(
-                encoding="utf-8"
+            manifest_version=manifest["manifest_version"],
+            name=manifest["name"],
+            uuid=manifest["uuid"],
+            url=manifest["url"],
+            default_sampling_rate=manifest["default_sampling_rate"],
+            icon=b64encode((self.root_dir / manifest["icon"]).read_bytes()).decode(
+                "utf-8"
+            ),
+            terms_of_service=(self.root_dir / manifest["terms_of_service"]).read_text(
+                "utf-8"
             ),
             update_infos=[
                 UpdateInfo(**update_info)
-                for update_info in json.load(
-                    (self.assets_dir / "update_infos.json").open(encoding="utf-8")
+                for update_info in json.loads(
+                    (self.root_dir / manifest["update_infos"]).read_text("utf-8")
                 )
             ],
             dependency_licenses=[
                 LicenseInfo(**license_info)
-                for license_info in json.load(
-                    (self.assets_dir / "dependency_licenses.json").open(
-                        encoding="utf-8"
-                    )
+                for license_info in json.loads(
+                    (self.root_dir / manifest["dependency_licenses"]).read_text("utf-8")
                 )
             ],
         )
