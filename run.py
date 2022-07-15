0db7aa5828f5dd231244e1e50307b92032e79f95
y-chan <y-chan@y-chan.dev>
2022-07-15 14:43:16 +0900

    fix port number

--- run.py
+++ run.py
@@ -751,7 +751,7 @@ if __name__ == "__main__":
     multiprocessing.freeze_support()
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default="127.0.0.1")
-    parser.add_argument("--port", type=int, default=50021)
+    parser.add_argument("--port", type=int, default=50025)
     parser.add_argument("--use_gpu", action="store_true")
     parser.add_argument("--sharevox_dir", type=Path, default=None)
     parser.add_argument("--voicelib_dir", type=Path, default=None, action="append")
