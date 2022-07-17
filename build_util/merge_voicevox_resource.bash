0269448ad5536f4c4fac878abd6697aa56796d56
Hiroshiba <hihokaruta@gmail.com>
2022-07-18 01:19:49 +0900

    merge voicevox resourceをスクリプト化

---
+++ build_util/merge_voicevox_resource.bash
@@ -0,0 +1,23 @@
+set -eux
+
+if [ -v "${DOWNLOAD_RESOURCE_PATH}" ]; then
+    echo "DOWNLOAD_RESOURCE_PATHが未定義です"
+    exit 1
+fi
+
+rm -r speaker_info
+cp -r $DOWNLOAD_RESOURCE_PATH/character_info speaker_info
+
+# マニフェスト
+jq -s '.[0] * .[1]' engine_manifest.json $DOWNLOAD_RESOURCE_PATH/engine/engine_manifest.json \
+    > engine_manifest.json.tmp
+mv engine_manifest.json.tmp engine_manifest.json
+
+python build_util/merge_update_infos.py \
+    engine_manifest_assets/update_infos.json \
+    $DOWNLOAD_RESOURCE_PATH/engine/engine_manifest_assets/update_infos.json \
+    engine_manifest_assets/update_infos.json
+
+for f in $(ls $DOWNLOAD_RESOURCE_PATH/engine/engine_manifest_assets/* | grep -v update_infos.json); do
+    cp $f ./engine_manifest_assets/
+done
