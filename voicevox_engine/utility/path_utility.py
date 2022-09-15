d0ecf41f6f7704a79fb78adcbaf2aadd0e0476b4
sabonerune <102559104+sabonerune@users.noreply.github.com>
2022-09-16 01:36:06 +0900

    ENH:ユーザー辞書のディレクトリを製品版と開発版で切り替える

--- voicevox_engine/utility/path_utility.py
+++ voicevox_engine/utility/path_utility.py
@@ -21,11 +21,31 @@ def engine_root() -> Path:
     return root_dir.resolve(strict=True)
 
 
+def is_development() -> bool:
+    """
+    開発版かどうか判定する関数
+    Nuitka/Pyinstallerでコンパイルされていない場合は開発環境とする。
+    """
+    # nuitkaビルドをした際はグローバルに__compiled__が含まれる
+    if "__compiled__" in globals():
+        return False
+
+    # pyinstallerでビルドをした際はsys.frozenが設定される
+    elif getattr(sys, "frozen", False):
+        return False
+
+    return True
+
+
 def get_save_dir():
     # FIXME: ファイル保存場所をエンジン固有のIDが入ったものにする
     # FIXME: Windowsは`voicevox-engine/voicevox-engine`ディレクトリに保存されているので
     # `VOICEVOX/voicevox-engine`に変更する
-    return Path(user_data_dir("voicevox-engine"))
+    if is_development():
+        app_name = "voicevox-engine-dev"
+    else:
+        app_name = "voicevox-engine"
+    return Path(user_data_dir(app_name))
 
 
 def delete_file(file_path: str) -> None:
