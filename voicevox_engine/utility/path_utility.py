65bc9f00fa22ba9053f2fa3fe5b8a57d61140292
sabonerune <102559104+sabonerune@users.noreply.github.com>
2022-09-20 20:46:13 +0900

    ENH:ユーザー辞書のディレクトリを製品版と開発版で切り替える (#474)
    * ENH:ユーザー辞書のディレクトリを製品版と開発版で切り替える
    
    * MAINT: engine_root()でもis_development()を使うように

--- voicevox_engine/utility/path_utility.py
+++ voicevox_engine/utility/path_utility.py
@@ -7,25 +7,41 @@ from appdirs import user_data_dir
 
 
 def engine_root() -> Path:
+    if is_development():
+        root_dir = Path(__file__).parents[2]
+
+    # Nuitka/Pyinstallerでビルドされている場合
+    else:
+        root_dir = Path(sys.argv[0]).parent
+
+    return root_dir.resolve(strict=True)
+
+
+def is_development() -> bool:
+    """
+    開発版かどうか判定する関数
+    Nuitka/Pyinstallerでコンパイルされていない場合は開発環境とする。
+    """
     # nuitkaビルドをした際はグローバルに__compiled__が含まれる
     if "__compiled__" in globals():
-        root_dir = Path(sys.argv[0]).parent
+        return False
 
     # pyinstallerでビルドをした際はsys.frozenが設定される
     elif getattr(sys, "frozen", False):
-        root_dir = Path(sys.argv[0]).parent
+        return False
 
-    else:
-        root_dir = Path(__file__).parents[2]
-
-    return root_dir.resolve(strict=True)
+    return True
 
 
 def get_save_dir():
     # FIXME: ファイル保存場所をエンジン固有のIDが入ったものにする
     # FIXME: Windowsは`voicevox-engine/voicevox-engine`ディレクトリに保存されているので
     # `VOICEVOX/voicevox-engine`に変更する
-    return Path(user_data_dir("voicevox-engine"))
+    if is_development():
+        app_name = "voicevox-engine-dev"
+    else:
+        app_name = "voicevox-engine"
+    return Path(user_data_dir(app_name))
 
 
 def delete_file(file_path: str) -> None:
