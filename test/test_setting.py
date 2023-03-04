b85748a27ed275741bc1b8971bbc75c8bf7f67a9
My <84212641+My-MC@users.noreply.github.com>
2023-03-04 09:06:31 +0900

    allow_originに文字列が含まれる場合のテストを追加

--- test/test_setting.py
+++ test/test_setting.py
@@ -41,6 +41,20 @@ class TestSettingLoader(TestCase):
             {"allow_origin": None, "cors_policy_mode": "all"},
         )
 
+    def test_loading_4(self):
+        setting_loader = SettingLoader(
+            setting_file_path=Path("test/setting-test-load-3.yaml")
+        )
+        settings = setting_loader.load_setting_file()
+
+        self.assertEqual(
+            settings.dict(),
+            {
+                "allow_origin": "192.168.254.255 192.168.255.255",
+                "cors_policy_mode": CorsPolicyMode.localapps,
+            },
+        )
+
     def test_dump(self):
         setting_loader = SettingLoader(
             setting_file_path=Path(self.tmp_dir_path / "setting-test-dump.yaml")
