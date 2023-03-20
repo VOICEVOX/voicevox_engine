13b29c839910b165f972b8c788943dedac553145
tungthncc <tungtranhuy@ncc.asia>
2023-03-20 08:58:35 +0700

    fix app init for docs generation

--- make_docs.py
+++ make_docs.py
@@ -3,14 +3,18 @@ import json
 from voicevox_engine.dev.core import mock as core
 from voicevox_engine.dev.synthesis_engine.mock import MockSynthesisEngine
 from voicevox_engine.setting import USER_SETTING_PATH, SettingLoader
+from huggingsound import SpeechRecognitionModel
 
 if __name__ == "__main__":
     import run
-
+    
+    default_model = "jonatasgrosman/wav2vec2-large-xlsr-53-japanese"
+    model = SpeechRecognitionModel(default_model)
     app = run.generate_app(
         synthesis_engines={"mock": MockSynthesisEngine(speakers=core.metas())},
         latest_core_version="mock",
         setting_loader=SettingLoader(USER_SETTING_PATH),
+        stt_model=model
     )
     with open("docs/api/index.html", "w") as f:
         f.write(
