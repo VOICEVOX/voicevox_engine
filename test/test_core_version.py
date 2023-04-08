96b591948ba7d5796a329e56a86de93c9797e8e9
aoirint <aoirint@gmail.com>
2023-04-08 10:52:44 +0900

    Update test/test_core_version.py
    Co-authored-by: Hiroshiba <hihokaruta@gmail.com>

--- test/test_core_version.py
+++ test/test_core_version.py
@@ -9,11 +9,11 @@ from voicevox_engine.utility.core_version import (
 class TestCoreVersion(TestCase):
     def test_parse_core_version(self):
         parse_core_version("0.1.0")
-        parse_core_version("0.10.preview.1")
+        parse_core_version("0.10-preview.1")
         parse_core_version("0.10.0")
         parse_core_version("0.14.0")
-        parse_core_version("0.14.0.preview.1")
-        parse_core_version("0.14.0.preview.10")
+        parse_core_version("0.14.0-preview.1")
+        parse_core_version("0.14.0-preview.10")
 
     def test_get_latest_core_version(self):
         self.assertEqual(
