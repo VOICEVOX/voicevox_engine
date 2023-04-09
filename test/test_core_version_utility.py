66c4daf6724231c8ceae102d522cf7d8594c54e2
aoirint <aoirint@gmail.com>
2023-04-09 23:44:54 +0900

    replace invalid semver in test: 0.10-preview.1 -> 0.10.0-preview.1

--- test/test_core_version_utility.py
+++ test/test_core_version_utility.py
@@ -9,8 +9,8 @@ from voicevox_engine.utility.core_version_utility import (
 class TestCoreVersion(TestCase):
     def test_parse_core_version(self):
         parse_core_version("0.1.0")
-        parse_core_version("0.10-preview.1")
         parse_core_version("0.10.0")
+        parse_core_version("0.10.0-preview.1")
         parse_core_version("0.14.0")
         parse_core_version("0.14.0-preview.1")
         parse_core_version("0.14.0-preview.10")
@@ -20,8 +20,8 @@ class TestCoreVersion(TestCase):
             get_latest_core_version(
                 versions=[
                     "0.1.0",
-                    "0.10-preview.1",
                     "0.10.0",
+                    "0.10.0-preview.1",
                     "0.14.0",
                     "0.14.0-preview.1",
                     "0.14.0-preview.10",
