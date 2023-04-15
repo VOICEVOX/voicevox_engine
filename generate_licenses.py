ef95834fa1acd51653eef5604c5e356f9318b683
aoirint <aoirint@gmail.com>
2023-04-15 10:38:17 +0900

    remove cython license (cython update fixed automatic license detection)

--- generate_licenses.py
+++ generate_licenses.py
@@ -216,11 +216,6 @@ def generate_licenses() -> List[License]:
                     "https://raw.githubusercontent.com/gsnedders/python-webencodings/fa2cb5d75ab41e63ace691bc0825d3432ba7d694/LICENSE"
                 ) as res:
                     license.text = res.read().decode()
-            elif license.name.lower() == "cython":
-                with urllib.request.urlopen(
-                    "https://raw.githubusercontent.com/cython/cython/56b30f2e2cbf964eb5b1d1ea4448963d58301035/LICENSE.txt"
-                ) as res:
-                    license.text = res.read().decode()
             elif license.name.lower() == "markupsafe":
                 with urllib.request.urlopen(
                     "https://raw.githubusercontent.com/pallets/markupsafe/afde0d72bdf3b558d91f42e65f1cb33c8b5a3cf6/LICENSE.rst"
