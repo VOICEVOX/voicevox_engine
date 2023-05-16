63d3238da86eb2386e1d2d5f55208c84723f2c09
Hiroshiba <hihokaruta@gmail.com>
2023-05-16 09:26:56 +0900

    辞書のファイルパスを変える (#5)

--- voicevox_engine/utility/path_utility.py
+++ voicevox_engine/utility/path_utility.py
@@ -38,9 +38,9 @@ def get_save_dir():
     # FIXME: Windowsは`voicevox-engine/voicevox-engine`ディレクトリに保存されているので
     # `VOICEVOX/voicevox-engine`に変更する
     if is_development():
-        app_name = "voicevox-engine-dev"
+        app_name = "voicevox-nemo-engine-dev"
     else:
-        app_name = "voicevox-engine"
+        app_name = "voicevox-nemo-engine"
     return Path(user_data_dir(app_name))
 
 
