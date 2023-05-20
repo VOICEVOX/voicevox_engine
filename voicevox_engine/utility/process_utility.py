a024206d4bbaf20d1477fd9f0970a75aca7428de
misogihagi <47350059+misogihagi@users.noreply.github.com>
2023-05-21 04:46:01 +0900

    Create process_utility.py

---
+++ voicevox_engine/utility/process_utility.py
@@ -0,0 +1,40 @@
+from typing import Optional
+from psutil import Process, AccessDenied, process_iter, net_connections
+
+
+def check_port_is_live(port: int) -> bool:
+    return port in list(map(lambda c:c.laddr.port, net_connections()))
+
+
+class GetProcessResult:
+    def __init__(
+        self, processes: Optional[list[Process]], has_checked_all: bool
+    ):
+        self.processes = processes
+        self.has_checked_all = has_checked_all
+
+
+def get_process_by_port(port: int) -> GetProcessResult:
+    processes = []
+    has_checked_all = True
+
+    def has_process_port(proc):
+        return (
+            len(
+                [
+                    conn
+                    for conn in proc.connections()
+                    if conn.laddr.port == port
+                ]
+            )
+            > 0
+        )
+
+    for proc in process_iter():
+        try:
+            if has_process_port(proc):
+                processes.append(Process(proc.pid))
+        except AccessDenied:
+            has_checked_all = False
+    return GetProcessResult(processes, has_checked_all)
+  
