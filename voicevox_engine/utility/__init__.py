c0518cfe21bf3ecf7ecd52fdda2903801942a921
misogihagi <47350059+misogihagi@users.noreply.github.com>
2023-05-21 04:47:39 +0900

    Update __init__.py

--- voicevox_engine/utility/__init__.py
+++ voicevox_engine/utility/__init__.py
@@ -6,6 +6,7 @@ from .connect_base64_waves import (
 from .core_version_utility import get_latest_core_version, parse_core_version
 from .mutex_utility import mutex_wrapper
 from .path_utility import delete_file, engine_root, get_save_dir
+from .process_utility import check_port_is_live, get_process_by_port
 
 __all__ = [
     "ConnectBase64WavesException",
@@ -17,4 +18,6 @@ __all__ = [
     "engine_root",
     "get_save_dir",
     "mutex_wrapper",
+    "check_port_is_live",
+    "get_process_by_port",
 ]
