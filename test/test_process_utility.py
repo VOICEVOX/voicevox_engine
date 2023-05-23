343b2d516e23455532723fa89288121e62ea7b9b
misogihagi <47350059+misogihagi@users.noreply.github.com>
2023-05-23 22:06:02 +0900

    Update test_process_utility.py

--- test/test_process_utility.py
+++ test/test_process_utility.py
@@ -1,5 +1,8 @@
-import pytest
 import subprocess
+import time
+
+import pytest
+
 from voicevox_engine.utility import get_process_by_port
 
 
@@ -7,8 +10,10 @@ def test_port_occupation():
     port = 8080
     proc = subprocess.Popen(["python", "-m", "http.server", str(port)])
 
+    # for http server listening
+    time.sleep(1)
+
     result = get_process_by_port(port).processes
 
-    proc.kill()
-
     assert len(result) > 0
+    proc.kill()
