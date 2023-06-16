1c10fe07a837d94b2886884e259b407683c0b992
Yuto Ashida <y-chan@y-chan.dev>
2023-06-17 03:17:23 +0900

    `uninstall library` APIの追加 (#692)

--- run.py
+++ run.py
@@ -860,6 +860,26 @@ def generate_app(
         )
         return Response(status_code=204)
 
+    @app.post(
+        "/uninstall_library/{library_uuid}",
+        status_code=204,
+        tags=["音声ライブラリ管理"],
+    )
+    def uninstall_library(library_uuid: str):
+        """
+        音声ライブラリをアンインストールします。
+
+        Parameters
+        ----------
+        library_uuid: str
+            音声ライブラリのID
+        """
+        manifest = engine_manifest_loader.load_manifest()
+        if not manifest.supported_features.manage_library:
+            raise HTTPException(status_code=404, detail="この機能は実装されていません")
+        library_manager.uninstall_library(library_uuid)
+        return Response(status_code=204)
+
     @app.post("/initialize_speaker", status_code=204, tags=["その他"])
     def initialize_speaker(
         speaker: int,
