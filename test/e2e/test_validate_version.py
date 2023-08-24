21c8147cd789ecf71f2b34f9d49c37a8e17bd827
Motoki Saito <stmtk13044032@gmail.com>
2023-08-24 15:34:17 +0900

    linterのエラーを消す

--- test/e2e/test_validate_version.py
+++ test/e2e/test_validate_version.py
@@ -1,7 +1,7 @@
 from generate_test_client import client
 
+
 def test_fetch_version_success():
     response = client.get("/version")
     assert response.status_code == 200
     assert isinstance(response.json(), str)
-
