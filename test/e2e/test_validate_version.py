2d70efdfb03e8f0626717058b680d75274e4ca57
Motoki Saito <stmtk13044032@gmail.com>
2023-08-25 01:42:49 +0900

    きちんとしたバージョンが取得できるかを確認する

--- test/e2e/test_validate_version.py
+++ test/e2e/test_validate_version.py
@@ -1,7 +1,7 @@
 from generate_test_client import client
-
+from voicevox_engine import __version__
 
 def test_fetch_version_success():
     response = client.get("/version")
     assert response.status_code == 200
-    assert isinstance(response.json(), str)
+    assert response.json() == __version__
