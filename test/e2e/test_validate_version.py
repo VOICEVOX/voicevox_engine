15c47d68f0c705468676dbf635f5de86706909df
Motoki saito <stmtk1@users.noreply.github.com>
2023-08-26 11:56:21 +0900

    versionを取得するe2eテストを書く (#732)

---
+++ test/e2e/test_validate_version.py
@@ -0,0 +1,9 @@
+from generate_test_client import client
+
+from voicevox_engine import __version__
+
+
+def test_fetch_version_success():
+    response = client.get("/version")
+    assert response.status_code == 200
+    assert response.json() == __version__
