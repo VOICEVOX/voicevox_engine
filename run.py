8758967741b510cceb6d4b11ab5dda1a002026cd
aoirint <aoirint@gmail.com>
2023-10-09 16:30:18 +0900

    /synthesis_morphing: return type hint

--- run.py
+++ run.py
@@ -560,7 +560,7 @@ def generate_app(
         target_speaker: int,
         morph_rate: float = Query(..., ge=0.0, le=1.0),  # noqa: B008
         core_version: Optional[str] = None,
-    ):
+    ) -> FileResponse:
         """
         指定された2人の話者で音声を合成、指定した割合でモーフィングした音声を得ます。
         モーフィングの割合は`morph_rate`で指定でき、0.0でベースの話者、1.0でターゲットの話者に近づきます。
