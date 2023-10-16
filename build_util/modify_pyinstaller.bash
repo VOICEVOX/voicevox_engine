791f4421855a732eb98de836b91c6f6dfdb2eb1b
aoirint <aoirint@gmail.com>
2023-10-16 19:08:33 +0900

    PyInstaller 5.13.2に更新 (#765)
    * Update to PyInstaller 5.13.2
    
    * update requirements.txt
    
    * modify_pyinstaller.bash: waf command: add --target-arch="64bit"
    
    * modify_pyinstaller.bash: waf command: --msvc_targets="x64"
    
    Thanks: https://github.com/VOICEVOX/voicevox_engine/issues/724
    
    * pyinstaller ^5.13

--- build_util/modify_pyinstaller.bash
+++ build_util/modify_pyinstaller.bash
@@ -21,5 +21,5 @@ __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
 __declspec(dllexport) DWORD AmdPowerXpressRequestHighPerformance = 0x00000001;
 #endif
 EOF
-(cd "$tempdir/bootloader" && python ./waf all)
+(cd "$tempdir/bootloader" && python ./waf all --msvc_targets="x64")
 pip install -U "$tempdir"
