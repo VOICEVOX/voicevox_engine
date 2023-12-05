2cb552ab091516403dc9d467e714f875b7720216
tarepan <tarepan5884@gmail.com>
2023-12-06 02:53:04 +0900

    修正: AquesTalk風記法の旧式記述を置換 (#818)
    Co-authored-by: Hiroshiba <hihokaruta@gmail.com>

--- voicevox_engine/kana_parser.py
+++ voicevox_engine/kana_parser.py
@@ -81,7 +81,7 @@ def _text_to_accent_phrase(phrase: str) -> AccentPhrase:
 
 def parse_kana(text: str) -> List[AccentPhrase]:
     """
-    AquesTalkライクな読み仮名をパースして音長・音高未指定のaccent phraseに変換
+    AquesTalk風記法テキストをパースして音長・音高未指定のaccent phraseに変換
     """
 
     parsed_results: List[AccentPhrase] = []
