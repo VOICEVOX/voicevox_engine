1b80016c92c55a41b03fd26b40bc89bb0ad35b48
tarepan <tarepan5884@gmail.com>
2023-12-11 08:57:24 +0000

    Refactor: `default_setting.yaml` の廃止

--- run.spec
+++ run.spec
@@ -10,7 +10,6 @@ datas = [
     ('default.csv', '.'),
     ('licenses.json', '.'),
     ('presets.yaml', '.'),
-    ('default_setting.yaml', '.'),
     ('ui_template', 'ui_template'),
 ]
 datas += collect_data_files('pyopenjtalk')
--- voicevox_engine/setting/SettingLoader.py
+++ voicevox_engine/setting/SettingLoader.py
@@ -2,21 +2,30 @@ from pathlib import Path
 
 import yaml
 
-from ..utility import engine_root, get_save_dir
+from ..utility import get_save_dir
 from .Setting import Setting
 
-DEFAULT_SETTING_PATH: Path = engine_root() / "default_setting.yaml"
 USER_SETTING_PATH: Path = get_save_dir() / "setting.yml"
 
 
 class SettingLoader:
     def __init__(self, setting_file_path: Path) -> None:
+        """
+        設定ファイルの管理
+        Parameters
+        ----------
+        setting_file_path : Path
+            設定ファイルのパス。存在しない場合はデフォルト値を設定。
+        """
         self.setting_file_path = setting_file_path
 
     def load_setting_file(self) -> Setting:
+        # 設定値の読み込み
         if not self.setting_file_path.is_file():
-            setting = yaml.safe_load(DEFAULT_SETTING_PATH.read_text(encoding="utf-8"))
+            # 設定ファイルが存在しないためデフォルト値を取得
+            setting = {"allow_origin": None, "cors_policy_mode": "localapps"}
         else:
+            # 指定された設定ファイルから値を取得
             setting = yaml.safe_load(self.setting_file_path.read_text(encoding="utf-8"))
 
         setting = Setting(
