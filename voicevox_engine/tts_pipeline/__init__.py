1360abb37d55538c16177d6473ef1332701cbbaf
tarepan <tarepan5884@gmail.com>
2023-12-13 20:21:22 +0000

    Refactor: `make_tts_engines.py` への改名

--- voicevox_engine/tts_pipeline/__init__.py
+++ voicevox_engine/tts_pipeline/__init__.py
@@ -1,5 +1,5 @@
 from ..core_wrapper import CoreWrapper, load_runtime_lib
-from .make_synthesis_engines import make_synthesis_engines
+from .make_tts_engines import make_synthesis_engines
 from .tts_engine import SynthesisEngine
 from .tts_engine_base import SynthesisEngineBase
 
