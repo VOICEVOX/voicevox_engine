b1fa87c67d39e43dc7a8510420f1ed1954caef48
tarepan <tarepan5884@gmail.com>
2023-12-13 14:51:39 +0000

    Refactor: APIドキュメント生成コードの段階化

--- build_util/make_docs.py
+++ build_util/make_docs.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 
 from voicevox_engine.dev.core import mock as core
 from voicevox_engine.dev.synthesis_engine.mock import MockTTSEngine
@@ -6,20 +7,11 @@ from voicevox_engine.preset import PresetManager
 from voicevox_engine.setting import USER_SETTING_PATH, SettingLoader
 from voicevox_engine.utility import engine_root
 
-if __name__ == "__main__":
-    import run
 
-    app = run.generate_app(
-        synthesis_engines={"mock": MockTTSEngine(speakers=core.metas())},
-        latest_core_version="mock",
-        setting_loader=SettingLoader(USER_SETTING_PATH),
-        preset_manager=PresetManager(  # FIXME: impl MockPresetManager
-            preset_path=engine_root() / "presets.yaml",
-        ),
-    )
-    with open("docs/api/index.html", "w") as f:
-        f.write(
-            """<!DOCTYPE html>
+def generate_api_docs_html(schema: str) -> str:
+    """OpenAPI schema から API ドキュメント HTML を生成する"""
+
+    return """<!DOCTYPE html>
 <html lang="ja">
 <head>
     <title>voicevox_engine API Document</title>
@@ -33,6 +25,29 @@ if __name__ == "__main__":
         Redoc.init(%s, {"hideHostname": true}, document.getElementById("redoc-container"));
     </script>
 </body>
-</html>"""
-            % json.dumps(app.openapi())
-        )
+</html>""" % schema
+
+
+if __name__ == "__main__":
+
+    import run
+
+    # FastAPI機能を用いた OpenAPI schema の生成
+    app = run.generate_app(
+        synthesis_engines={"mock": MockTTSEngine(speakers=core.metas())},
+        latest_core_version="mock",
+        setting_loader=SettingLoader(USER_SETTING_PATH),
+        preset_manager=PresetManager(  # FIXME: impl MockPresetManager
+            preset_path=engine_root() / "presets.yaml",
+        ),
+    )
+    api_schema = json.dumps(app.openapi())
+
+    # APIドキュメントHTMLを生成する
+    api_docs_html = generate_api_docs_html(api_schema)
+
+    # HTMLファイルとして保存する
+    api_docs_root = Path("docs/api") # 'upload-docs' workflow の対象
+    output_path = api_docs_root / "index.html"
+    with open(output_path, "w") as f:
+        f.write(api_docs_html)
