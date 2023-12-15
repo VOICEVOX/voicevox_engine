ec1f70e52b1df5628317311fced841f4e9d8877c
tarepan <tarepan5884@gmail.com>
2023-12-16 01:05:09 +0900

    廃止: `MetasStore` の不使用関数 (#875)

--- voicevox_engine/metas/MetasStore.py
+++ voicevox_engine/metas/MetasStore.py
@@ -20,7 +20,6 @@ class MetasStore:
         engine_speakers_path : Path
             エンジンに含まれる話者メタ情報ディレクトリのパス。
         """
-        self._engine_speakers_path = engine_speakers_path
         # エンジンに含まれる各話者のメタ情報
         self._loaded_metas: Dict[str, EngineSpeaker] = {
             folder.name: EngineSpeaker(
@@ -28,41 +27,6 @@ class MetasStore:
             )
             for folder in engine_speakers_path.iterdir()
         }
-
-    def speaker_engine_metas(self, speaker_uuid: str) -> EngineSpeaker:
-        """
-        エンジンに含まれる指定話者のメタ情報を取得
-        Parameters
-        ----------
-        speaker_uuid : str
-            話者UUID
-        Returns
-        -------
-        ret : EngineSpeaker
-            エンジンに含まれる指定話者のメタ情報
-        """
-        return self.loaded_metas[speaker_uuid]
-
-    def combine_metas(self, core_metas: List[CoreSpeaker]) -> List[Speaker]:
-        """
-        コアに含まれる話者メタ情報に、エンジンに含まれる話者メタ情報を統合して返す
-        Parameters
-        ----------
-        core_metas : List[CoreSpeaker]
-            コアに含まれる話者メタ情報
-        Returns
-        -------
-        ret : List[Speaker]
-            エンジンとコアに含まれる話者メタ情報
-        """
-        # 話者単位でエンジン・コアに含まれるメタ情報を統合
-        return [
-            Speaker(
-                **self.speaker_engine_metas(speaker_meta.speaker_uuid).dict(),
-                **speaker_meta.dict(),
-            )
-            for speaker_meta in core_metas
-        ]
 
     # FIXME: engineではなくList[CoreSpeaker]を渡す形にすることで
     # SynthesisEngineBaseによる循環importを修正する
@@ -81,15 +45,13 @@ class MetasStore:
         # コアに含まれる話者メタ情報の収集
         core_metas = [CoreSpeaker(**speaker) for speaker in json.loads(engine.speakers)]
         # エンジンに含まれる話者メタ情報との統合
-        return self.combine_metas(core_metas)
-
-    @property
-    def engine_speakers_path(self) -> Path:
-        return self._engine_speakers_path
-
-    @property
-    def loaded_metas(self) -> Dict[str, EngineSpeaker]:
-        return self._loaded_metas
+        return [
+            Speaker(
+                **self.self._loaded_metas[speaker_meta.speaker_uuid].dict(),
+                **speaker_meta.dict(),
+            )
+            for speaker_meta in core_metas
+        ]
 
 
 def construct_lookup(speakers: List[Speaker]) -> Dict[int, Tuple[Speaker, StyleInfo]]:
