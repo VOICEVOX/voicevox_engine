5edb7ee29905d34256f5beb9638806cafbf0db94
tarepan <tarepan5884@gmail.com>
2023-12-22 12:36:59 +0000

    add: コア Mock とその受け入れ

--- voicevox_engine/dev/core/__init__.py
+++ voicevox_engine/dev/core/__init__.py
@@ -1,17 +1,3 @@
-from .mock import (
-    decode_forward,
-    initialize,
-    metas,
-    supported_devices,
-    yukarin_s_forward,
-    yukarin_sa_forward,
-)
+from .mock import MockCore
 
-__all__ = [
-    "decode_forward",
-    "initialize",
-    "yukarin_s_forward",
-    "yukarin_sa_forward",
-    "metas",
-    "supported_devices",
-]
+__all__ = ["MockCore"]
