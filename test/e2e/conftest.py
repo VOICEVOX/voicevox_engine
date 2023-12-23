d328d92271b50df677a1999aa7d0652dc1b4f299
Hiroshiba <hihokaruta@gmail.com>
2023-12-23 15:32:58 +0900

    `/speakers` APIを叩くe2eテストを追加 (#925)
    * speakersAPIを叩くe2eテストを追加
    
    * Add syrupy package to requirements-test.txt
    
    * ^

--- test/e2e/conftest.py
+++ test/e2e/conftest.py
@@ -3,11 +3,26 @@ from pathlib import Path
 import pytest
 from fastapi.testclient import TestClient
 from run import generate_app
+from syrupy.assertion import SnapshotAssertion
+from syrupy.extensions.json import JSONSnapshotExtension
 
 from voicevox_engine.preset import PresetManager
 from voicevox_engine.setting import SettingLoader
 from voicevox_engine.tts_pipeline import make_synthesis_engines
 from voicevox_engine.utility.core_version_utility import get_latest_core_version
+
+
+@pytest.fixture
+def snapshot_json(snapshot: SnapshotAssertion):
+    """
+    syrupyでJSONをsnapshotするためのfixture。
+
+    Examples
+    --------
+    >>> def test_foo(snapshot_json: JSONSnapshotExtension):
+    >>>     assert snapshot_json == {"key": "value"}
+    """
+    return snapshot.use_extension(JSONSnapshotExtension)
 
 
 @pytest.fixture(scope="session")
