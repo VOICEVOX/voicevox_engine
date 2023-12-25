e133bdd5eeb713a842ceb2042b358a079d6161ba
terepan <tarepan5884@gmail.com>
2023-12-25 09:30:29 +0000

    fix: 移植忘れ

--- voicevox_engine/dev/synthesis_engine/mock.py
+++ voicevox_engine/dev/synthesis_engine/mock.py
@@ -1,3 +1,4 @@
+import copy
 from logging import getLogger
 from typing import Any, Dict, List
 
@@ -86,6 +87,8 @@ class MockTTSEngine(TTSEngineBase):
         enable_interrogative_upspeak: bool = True,
     ) -> np.ndarray:
         """音声合成用のクエリに含まれる読み仮名に基づいてOpenJTalkで音声波形を生成する (Mock)"""
+        # モーフィング時などに同一参照のqueryで複数回呼ばれる可能性があるので、元の引数のqueryに破壊的変更を行わない
+        query = copy.deepcopy(query)
 
         # recall text in katakana
         flatten_moras = to_flatten_moras(query.accent_phrases)
