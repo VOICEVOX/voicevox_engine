28be760a826610cda1ccd7154c8d1d34978c8e6a
sabonerune <102559104+sabonerune@users.noreply.github.com>
2023-12-28 07:40:12 +0900

    FIX: 型関係のバグを修正

--- voicevox_engine/utility/core_version_utility.py
+++ voicevox_engine/utility/core_version_utility.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Sequence
 
 from semver.version import Version
 
@@ -7,7 +7,7 @@ def parse_core_version(version: str) -> Version:
     return Version.parse(version)
 
 
-def get_latest_core_version(versions: Iterable[str]) -> str:
+def get_latest_core_version(versions: Sequence[str]) -> str:
     if len(versions) == 0:
         raise Exception("versions must be non-empty.")
 
