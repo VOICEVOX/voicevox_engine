e13733f9b7e6f79626b0b8be30f90a481537ee00
terepan <tarepan5884@gmail.com>
2023-12-31 05:25:08 +0000

    fix: lint

--- voicevox_engine/tts_pipeline/text_analyzer.py
+++ voicevox_engine/tts_pipeline/text_analyzer.py
@@ -82,7 +82,7 @@ class Label:
     @property
     def phoneme(self) -> OjtPhoneme:
         """このラベルに含まれる音素。子音 or 母音 (無音含む)。"""
-        return self.contexts["p3"] # type: ignore
+        return self.contexts["p3"]  # type: ignore
 
     def is_pause(self):
         """このラベルが無音 (silent/pause) であれば True、そうでなければ False を返す"""
