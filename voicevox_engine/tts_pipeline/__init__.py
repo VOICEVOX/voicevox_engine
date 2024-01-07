f16b793c183d000fbb6833bbfc8b47902881ef16
terepan <tarepan5884@gmail.com>
2024-01-07 13:34:35 +0000

    remove: __init__.py `load_runtime_lib`

--- voicevox_engine/tts_pipeline/__init__.py
+++ voicevox_engine/tts_pipeline/__init__.py
@@ -1,8 +1,7 @@
-from ..core_wrapper import CoreWrapper, load_runtime_lib
+from ..core_wrapper import CoreWrapper
 from .tts_engine import TTSEngine
 
 __all__ = [
     "CoreWrapper",
-    "load_runtime_lib",
     "TTSEngine",
 ]
