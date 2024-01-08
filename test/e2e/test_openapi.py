de3ad163b9882ecb69b94d44122e9cef3475e524
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2024-01-08 19:27:36 +0900

    コメント追加

--- test/e2e/test_openapi.py
+++ test/e2e/test_openapi.py
@@ -5,4 +5,5 @@ from syrupy.extensions.json import JSONSnapshotExtension
 def test_OpenAPIの形が変わっていないことを確認(
     app: FastAPI, snapshot_json: JSONSnapshotExtension
 ) -> None:
+    # 変更があった場合はREADMEの「スナップショットの更新」の手順で更新可能
     assert snapshot_json == app.openapi()
