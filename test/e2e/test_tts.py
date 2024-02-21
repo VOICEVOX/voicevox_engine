223118d5bcb3daee443926d268a5208f9dae0589
tarepan <tarepan5884@gmail.com>
2024-02-21 11:58:33 +0000

    fix: lint

--- test/e2e/test_tts.py
+++ test/e2e/test_tts.py
@@ -12,7 +12,9 @@ def test_テキストと話者IDから音声を合成できる(
     client: TestClient, snapshot_json: SnapshotAssertion
 ) -> None:
     # テキストと話者 ID から AudioQuery を生成する
-    audio_query_res = client.post("/audio_query", params={"text": "テストです", "speaker": 0})
+    audio_query_res = client.post(
+        "/audio_query", params={"text": "テストです", "speaker": 0}
+    )
     audio_query = audio_query_res.json()
 
     # AudioQuery から音声波形を生成する
