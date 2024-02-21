cdea0788ccc00e98a1c92f053b7263fe5c1d78e9
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2024-02-21 13:19:08 +0900

    snapshotへ

--- test/e2e/test_setting.py
+++ test/e2e/test_setting.py
@@ -6,8 +6,7 @@ from fastapi.testclient import TestClient
 from syrupy.assertion import SnapshotAssertion
 
 
-def test_setting画面が取得できる(client: TestClient, snapshot_json: SnapshotAssertion) -> None:
+def test_setting画面が取得できる(client: TestClient, snapshot: SnapshotAssertion) -> None:
     response = client.get("/setting")
     assert response.status_code == 200
-    print(response.content)
-    assert snapshot_json == response.content
+    assert snapshot == response.content.decode("utf-8")
