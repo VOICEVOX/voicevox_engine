b8b7adf094284a2c9ddf2b888ea6d6f96ca20934
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2024-02-21 13:25:51 +0900

    use hash_long_string

--- test/e2e/test_engine_manifest.py
+++ test/e2e/test_engine_manifest.py
@@ -2,6 +2,8 @@
 /engine_manifest APIのテスト
 """
 
+from test.utility import hash_long_string
+
 from fastapi.testclient import TestClient
 from syrupy.assertion import SnapshotAssertion
 
@@ -9,4 +11,4 @@ from syrupy.assertion import SnapshotAssertion
 def test_エンジンマニフェストを取得できる(client: TestClient, snapshot_json: SnapshotAssertion) -> None:
     response = client.get("/engine_manifest")
     assert response.status_code == 200
-    assert snapshot_json == response.json()
+    assert snapshot_json == hash_long_string(response.json())
