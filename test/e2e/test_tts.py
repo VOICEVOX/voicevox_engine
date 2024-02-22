f04adb68824b152adac60f281a5c36b984d370fe
tarepan <tarepan5884@gmail.com>
2024-02-22 05:01:31 +0000

    fix: Linux-Windows 数値誤差ワークアラウンドの追加

--- test/e2e/test_tts.py
+++ test/e2e/test_tts.py
@@ -2,8 +2,10 @@
 TTSのテスト
 """
 
-from test.utility import hash_long_string
+from test.utility import hash_long_string, round_floats
 
+import io
+import soundfile as sf
 from fastapi.testclient import TestClient
 from syrupy.assertion import SnapshotAssertion
 
@@ -20,8 +22,17 @@ def test_テキストと話者IDから音声を合成できる(
     # AudioQuery から音声波形を生成する
     synthesis_res = client.post("/synthesis", params={"speaker": 0}, json=audio_query)
 
-    # FileResponse の raw コンテンツを文字列として取得する
-    wave = str(synthesis_res.content)
+    # wav ファイルを含む FileResponse から音声波形を抽出する
+    wav_bytes = io.BytesIO(synthesis_res.read())
+    wave = sf.read(wav_bytes)[0].tolist()
 
+    # NOTE: Linux-Windows 数値精度問題に対するワークアラウンド
+    wave = round_floats(wave, 2)
+
+    # リクエストが成功している
     assert synthesis_res.status_code == 200
-    assert snapshot_json == hash_long_string(wave)
+    # レスポンスが音声ファイルである
+    assert synthesis_res.headers['content-type'] == 'audio/wav'
+    # 音声波形が commit 間で不変である
+    wave_str = " ".join(map(lambda point: str(point), wave))
+    assert snapshot_json == hash_long_string(wave_str)
