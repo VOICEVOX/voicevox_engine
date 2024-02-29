ff452bf7f7d48ec3666ff3cf8003d11d1fa944f8
tarepan <tarepan5884@gmail.com>
2024-02-29 03:10:03 +0000

    fix: テストスキップ形式

--- test/e2e/single_api/test_audio_query_from_preset.py
+++ test/e2e/single_api/test_audio_query_from_preset.py
@@ -2,12 +2,16 @@
 /audio_query_from_preset API のテスト
 """
 
+import pytest
 from fastapi.testclient import TestClient
 
-# def test_post_audio_query_from_preset_200(client: TestClient) -> None:
-#     query_params = {"text": "テストです", "preset_id": 0}
-#     response = client.post("/audio_query_from_preset", params=query_params)
-#     assert response.status_code == 200
+
+@pytest.mark.skip(reason="200の前提として別APIを要するプリセット登録が必要だから")
+def test_post_audio_query_from_preset_200(client: TestClient) -> None:
+    response = client.post(
+        "/audio_query_from_preset", params={"text": "テストです", "preset_id": 0}
+    )
+    assert response.status_code == 200
 
 
 def test_post_audio_query_from_preset_422(client: TestClient) -> None:
