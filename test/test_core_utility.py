422eea3a7235d928e7fe9a7fd2af846ab437e4da
tomoish <tomo.i.120608@gmail.com>
2024-03-08 17:45:54 +0900

    Fix quote

--- test/test_core_utility.py
+++ test/test_core_utility.py
@@ -5,14 +5,14 @@ from voicevox_engine.utility.core_utility import get_half_logical_cores
 
 
 class TestHalfLogicalCores(TestCase):
-    @patch('os.cpu_count', return_value=8)
+    @patch("os.cpu_count", return_value=8)
     def test_half_logical_cores_even(self, mock_cpu_count):
         self.assertEqual(get_half_logical_cores(), 4)
 
-    @patch('os.cpu_count', return_value=9)
+    @patch("os.cpu_count", return_value=9)
     def test_half_logical_cores_odd(self, mock_cpu_count):
         self.assertEqual(get_half_logical_cores(), 4)
 
-    @patch('os.cpu_count', return_value=None)
+    @patch("os.cpu_count", return_value=None)
     def test_half_logical_cores_none(self, mock_cpu_count):
         self.assertEqual(get_half_logical_cores(), 0)
