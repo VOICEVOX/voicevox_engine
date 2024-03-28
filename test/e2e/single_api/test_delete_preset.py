13b601a24047767eff2ee735a72393495ca898f6
tarepan <tarepan5884@gmail.com>
2024-03-28 15:46:30 +0000

    add: `POST /delete_preset 422` テストを追加

--- test/e2e/single_api/test_delete_preset.py
+++ test/e2e/single_api/test_delete_preset.py
@@ -14,3 +14,11 @@ def test_post_delete_preset_204(
     response = client.post("/delete_preset", params={"id": 1})
     assert response.status_code == 204
     assert snapshot_json == response.json()
+
+
+def test_post_delete_preset_422(
+    client: TestClient, snapshot_json: SnapshotAssertion
+) -> None:
+    response = client.post("/delete_preset", params={"id": 4040000000})
+    assert response.status_code == 422
+    assert snapshot_json == response.json()
