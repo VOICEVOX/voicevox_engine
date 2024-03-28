68414bde692fa94c479cee389df71be6ae2df4b7
tarepan <tarepan5884@gmail.com>
2024-03-28 15:53:46 +0000

    add: `POST update_preset 422` テストを追加

--- test/e2e/single_api/test_update_preset.py
+++ test/e2e/single_api/test_update_preset.py
@@ -5,6 +5,8 @@
 import pytest
 from fastapi.testclient import TestClient
 
+from syrupy.assertion import SnapshotAssertion
+
 
 @pytest.mark.skip(reason="プリセット変更が他のテストに干渉するから")
 def test_post_update_preset_200(client: TestClient) -> None:
@@ -22,3 +24,23 @@ def test_post_update_preset_200(client: TestClient) -> None:
     }
     response = client.post("/update_preset", params={}, json=preset)
     assert response.status_code == 200
+
+
+def test_post_update_preset_422(
+    client: TestClient, snapshot_json: SnapshotAssertion
+) -> None:
+    preset = {
+        "id": 4040000000,
+        "name": "Nessie",
+        "speaker_uuid": "404-404-404-404",
+        "style_id": 404,
+        "speedScale": 404,
+        "pitchScale": 404,
+        "intonationScale": 404,
+        "volumeScale": 404,
+        "prePhonemeLength": 404,
+        "postPhonemeLength": 404,
+    }
+    response = client.post("/update_preset", params={}, json=preset)
+    assert response.status_code == 422
+    assert snapshot_json == response.json()
