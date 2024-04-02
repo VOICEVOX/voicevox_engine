bed92e72c2bfd2cfa395c7ffa03b8f96f5441291
tarepan <tarepan5884@gmail.com>
2024-04-02 12:59:51 +0000

    fix: lint

--- test/e2e/single_api/test_get_root.py
+++ test/e2e/single_api/test_get_root.py
@@ -6,9 +6,7 @@ from fastapi.testclient import TestClient
 from syrupy.assertion import SnapshotAssertion
 
 
-def test_get_root_200(
-    client: TestClient, snapshot: SnapshotAssertion
-) -> None:
+def test_get_root_200(client: TestClient, snapshot: SnapshotAssertion) -> None:
     response = client.get("/")
     assert response.status_code == 200
     assert snapshot == response.content.decode("utf-8")
