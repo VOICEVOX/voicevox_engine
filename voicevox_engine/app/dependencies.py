6f5d4fbb23e584638ea4d637c577285ee36215a5
tarepan <tarepan5884@gmail.com>
2024-04-09 04:24:36 +0000

    fix: グローバル変数 FIXME 追加

--- voicevox_engine/app/dependencies.py
+++ voicevox_engine/app/dependencies.py
@@ -9,11 +9,12 @@ class MutableAPI:
     enable: bool = True
 
 
-mutable_api = MutableAPI()
+# FIXME: グローバル変数が複数ファイルに分散しているため、DI 等で局所化する
+deprecated_mutable_api = MutableAPI()
 
 
 async def check_disabled_mutable_api() -> None:
-    if not mutable_api.enable:
+    if not deprecated_mutable_api.enable:
         raise HTTPException(
             status_code=403,
             detail="エンジンの静的なデータを変更するAPIは無効化されています",
