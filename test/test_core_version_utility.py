d7b10f704cf1b93ec82bea2f32b94adcc95d0928
tarepan <tarepan5884@gmail.com>
2024-05-02 09:26:01 +0000

    refactor: コアバージョン関連テストの形式を更新

--- test/test_core_version_utility.py
+++ test/test_core_version_utility.py
@@ -1,31 +1,48 @@
-from unittest import TestCase
+"""
+コアバージョンに関する utilities のテスト
+"""
 
 from voicevox_engine.utility.core_version_utility import get_latest_core_version
 
 
-class TestCoreVersion(TestCase):
-    def test_get_latest_core_version(self) -> None:
-        self.assertEqual(
-            get_latest_core_version(
-                [
-                    "0.0.0",
-                    "0.1.0",
-                    "0.10.0",
-                    "0.10.0-preview.1",
-                    "0.14.0",
-                    "0.14.0-preview.1",
-                    "0.14.0-preview.10",
-                ]
-            ),
-            "0.14.0",
-        )
+def test_get_latest_core_version_same_preview_normal(self) -> None:
+    """`get_latest_core_version()` は同一バージョンの preview 版より正規版を優先する。"""
 
-        self.assertEqual(
-            get_latest_core_version(
-                [
-                    "0.14.0",
-                    "0.15.0-preview.1",
-                ]
-            ),
-            "0.15.0-preview.1",
-        )
+    # Inputs
+    versions = [
+        "0.0.0",
+        "0.1.0",
+        "0.10.0",
+        "0.10.0-preview.1",
+        "0.14.0",
+        "0.14.0-preview.1",
+        "0.14.0-preview.10",
+    ]
+
+    # Expects
+    true_latest = "0.14.0"
+
+    # Outputs
+    latest = get_latest_core_version(versions)
+
+    # Tests
+    assert true_latest == latest
+
+
+def test_get_latest_core_version_newer_preview(self) -> None:
+    """`get_latest_core_version()` は旧バージョンの正規版より新バージョンの preview 版を優先する。"""
+
+    # Inputs
+    versions = [
+        "0.14.0",
+        "0.15.0-preview.1",
+    ]
+
+    # Expects
+    true_latest = "0.15.0-preview.1"
+
+    # Outputs
+    latest = get_latest_core_version(versions)
+
+    # Tests
+    assert true_latest == latest
