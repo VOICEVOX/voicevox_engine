55e98046c2b0c074c770efec6057cd1ce5a6c419
tarepan <tarepan5884@gmail.com>
2024-05-02 09:15:19 +0000

    refactor: `parse_core_version()` 削除

--- test/test_core_version_utility.py
+++ test/test_core_version_utility.py
@@ -1,21 +1,9 @@
 from unittest import TestCase
 
-from voicevox_engine.utility.core_version_utility import (
-    get_latest_core_version,
-    parse_core_version,
-)
+from voicevox_engine.utility.core_version_utility import get_latest_core_version
 
 
 class TestCoreVersion(TestCase):
-    def test_parse_core_version(self) -> None:
-        parse_core_version("0.0.0")
-        parse_core_version("0.1.0")
-        parse_core_version("0.10.0")
-        parse_core_version("0.10.0-preview.1")
-        parse_core_version("0.14.0")
-        parse_core_version("0.14.0-preview.1")
-        parse_core_version("0.14.0-preview.10")
-
     def test_get_latest_core_version(self) -> None:
         self.assertEqual(
             get_latest_core_version(
--- voicevox_engine/utility/core_version_utility.py
+++ voicevox_engine/utility/core_version_utility.py
@@ -1,14 +1,7 @@
-from collections.abc import Collection
-
 from semver.version import Version
 
 
-def parse_core_version(version: str) -> Version:
-    return Version.parse(version)
-
-
-def get_latest_core_version(versions: Collection[str]) -> str:
+def get_latest_core_version(versions: list[str]) -> str:
     if len(versions) == 0:
         raise Exception("versions must be non-empty.")
-
-    return str(max(map(parse_core_version, versions)))
+    return str(max(map(Version.parse, versions)))
