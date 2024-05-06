363adde999194940eff435d0222c856eeedcec7a
tarepan <tarepan5884@gmail.com>
2024-05-06 09:41:54 +0000

    refactor: リソース処理のコメントを追加

--- build_util/process_voicevox_resource.bash
+++ build_util/process_voicevox_resource.bash
@@ -1,27 +1,31 @@
 set -eux
 
+# リソースディレクトリのパスを検証する
 if [ ! -v DOWNLOAD_RESOURCE_PATH ]; then
     echo "DOWNLOAD_RESOURCE_PATHが未定義です"
     exit 1
 fi
 
+# ダミーのキャラクター情報を製品版のキャラクター情報で置き換える
 rm -r speaker_info
 cp -r "${DOWNLOAD_RESOURCE_PATH}/character_info" speaker_info
 
-# キャラクター情報の前処理をする
+# キャラクター情報を前処理する
 python "${DOWNLOAD_RESOURCE_PATH}/scripts/clean_character_info.py" \
     --character_info_dir speaker_info/
 
-# マニフェスト
+# エンジンマニフェストに含まれるダミーの情報を製品版の情報で上書きする
 jq -s '.[0] * .[1]' engine_manifest.json "${DOWNLOAD_RESOURCE_PATH}/engine/engine_manifest.json" \
     > engine_manifest.json.tmp
 mv engine_manifest.json.tmp engine_manifest.json
 
+# エンジンのアップデート情報へリソースのアップデート情報を追加する
 python build_util/merge_update_infos.py \
     engine_manifest_assets/update_infos.json \
     "${DOWNLOAD_RESOURCE_PATH}/engine/engine_manifest_assets/update_infos.json" \
     engine_manifest_assets/update_infos.json
 
+# エンジンのディレクトリへリソースのマニフェストアセットを複製する
 for f in $(ls "${DOWNLOAD_RESOURCE_PATH}"/engine/engine_manifest_assets/* | grep -v update_infos.json); do
     cp "${f}" ./engine_manifest_assets/
 done
