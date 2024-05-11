2d3a91c009262722d8d47fe5cbddf5ad6391a128
tarepan <tarepan5884@gmail.com>
2024-05-11 13:20:49 +0000

    refactor: `EngineManifestLoader` を削除して簡略化

--- voicevox_engine/app/application.py
+++ voicevox_engine/app/application.py
@@ -19,7 +19,7 @@ from voicevox_engine.app.routers.tts_pipeline import generate_tts_pipeline_route
 from voicevox_engine.app.routers.user_dict import generate_user_dict_router
 from voicevox_engine.cancellable_engine import CancellableEngine
 from voicevox_engine.core.core_adapter import CoreAdapter
-from voicevox_engine.engine_manifest.EngineManifestLoader import EngineManifestLoader
+from voicevox_engine.engine_manifest.EngineManifestLoader import load_manifest
 from voicevox_engine.library_manager import LibraryManager
 from voicevox_engine.metas.MetasStore import MetasStore
 from voicevox_engine.preset.PresetManager import PresetManager
@@ -62,9 +62,7 @@ def generate_app(
     if disable_mutable_api:
         deprecated_mutable_api.enable = False
 
-    engine_manifest_data = EngineManifestLoader(
-        engine_root() / "engine_manifest.json", engine_root()
-    ).load_manifest()
+    engine_manifest_data = load_manifest(engine_root() / "engine_manifest.json")
     library_manager = LibraryManager(
         get_save_dir() / "installed_libraries",
         engine_manifest_data.supported_vvlib_manifest_version,
