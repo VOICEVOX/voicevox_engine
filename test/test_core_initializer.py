0dcda604570ebc0616017e749d31d8109c20bf15
tarepan <tarepan5884@gmail.com>
2024-05-18 15:11:11 +0000

    refactor: misc テストを pytest 化

--- test/test_core_initializer.py
+++ test/test_core_initializer.py
@@ -1,18 +1,16 @@
-from unittest import TestCase
 from unittest.mock import patch
 
 from voicevox_engine.core.core_initializer import get_half_logical_cores
 
 
-class TestHalfLogicalCores(TestCase):
-    @patch("os.cpu_count", return_value=8)
-    def test_half_logical_cores_even(self, mock_cpu_count: int) -> None:
-        self.assertEqual(get_half_logical_cores(), 4)
+@patch("os.cpu_count", return_value=8)
+def test_half_logical_cores_even(mock_cpu_count: int) -> None:
+    assert get_half_logical_cores() == 4
 
-    @patch("os.cpu_count", return_value=9)
-    def test_half_logical_cores_odd(self, mock_cpu_count: int) -> None:
-        self.assertEqual(get_half_logical_cores(), 4)
+@patch("os.cpu_count", return_value=9)
+def test_half_logical_cores_odd(mock_cpu_count: int) -> None:
+    assert get_half_logical_cores() == 4
 
-    @patch("os.cpu_count", return_value=None)
-    def test_half_logical_cores_none(self, mock_cpu_count: int) -> None:
-        self.assertEqual(get_half_logical_cores(), 0)
+@patch("os.cpu_count", return_value=None)
+def test_half_logical_cores_none(mock_cpu_count: int) -> None:
+    assert get_half_logical_cores() == 0
