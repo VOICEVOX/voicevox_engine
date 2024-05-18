e49f95bd42cf62d00052937f42c9e867a41d19d6
tarepan <tarepan5884@gmail.com>
2024-05-18 13:25:36 +0000

    refactor: word_type テストを pytest 化

--- test/user_dict/test_word_types.py
+++ test/user_dict/test_word_types.py
@@ -4,6 +4,7 @@ from voicevox_engine.model import WordTypes
 from voicevox_engine.user_dict.part_of_speech_data import part_of_speech_data
 
 
-class TestWordTypes(TestCase):
-    def test_word_types(self) -> None:
-        self.assertCountEqual(list(WordTypes), list(part_of_speech_data.keys()))
+def test_word_types() -> None:
+    word_types = list(WordTypes)
+    part_of_speeches = list(part_of_speech_data.keys())
+    assert sorted(word_types) == sorted(part_of_speeches)
