0dc54bb84abe48d7cf50c2bd0439e130f4f1845f
tarepan <tarepan5884@gmail.com>
2024-05-23 04:47:12 +0000

    fix: コンフリクト

--- test/benchmark/engine_preparation.py
+++ test/benchmark/engine_preparation.py
@@ -10,23 +10,25 @@ from fastapi.testclient import TestClient
 from voicevox_engine.app.application import generate_app
 from voicevox_engine.core.core_initializer import initialize_cores
 from voicevox_engine.preset.PresetManager import PresetManager
-from voicevox_engine.setting.SettingLoader import SettingHandler
+from voicevox_engine.setting.Setting import SettingHandler
 from voicevox_engine.tts_pipeline.tts_engine import make_tts_engines_from_cores
 from voicevox_engine.user_dict.user_dict import UserDictionary
 from voicevox_engine.utility.core_version_utility import get_latest_version
 
 
 def _generate_engine_fake_server(root_dir: Path) -> TestClient:
-    cores = initialize_cores(voicevox_dir=root_dir, use_gpu=False, enable_mock=False)
-    tts_engines = make_tts_engines_from_cores(cores)
-    latest_core_version = get_latest_version(list(tts_engines.keys()))
+    core_manager = initialize_cores(
+        voicevox_dir=root_dir, use_gpu=False, enable_mock=False
+    )
+    tts_engines = make_tts_engines_from_cores(core_manager)
+    latest_core_version = get_latest_version(tts_engines.versions())
     setting_loader = SettingHandler(Path("./not_exist.yaml"))
     preset_manager = PresetManager(Path("./presets.yaml"))
     user_dict = UserDictionary()
 
     app = generate_app(
         tts_engines=tts_engines,
-        cores=cores,
+        core_manager=core_manager,
         latest_core_version=latest_core_version,
         setting_loader=setting_loader,
         preset_manager=preset_manager,
