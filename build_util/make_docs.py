86df79c63b831b42112b3f25ec7c49822c380bfa
tarepan <tarepan5884@gmail.com>
2024-05-28 15:15:31 +0900

    整理: マニフェストパス生成関数を追加 (#1341)
    * refactor: マニフェストパス生成関数を追加
    
    * fix: lint

--- build_util/make_docs.py
+++ build_util/make_docs.py
@@ -11,7 +11,7 @@ from voicevox_engine.preset.PresetManager import PresetManager
 from voicevox_engine.setting.Setting import USER_SETTING_PATH, SettingHandler
 from voicevox_engine.tts_pipeline.tts_engine import TTSEngineManager
 from voicevox_engine.user_dict.user_dict import UserDictionary
-from voicevox_engine.utility.path_utility import engine_root
+from voicevox_engine.utility.path_utility import engine_manifest_path, engine_root
 
 
 def generate_api_docs_html(schema: str) -> str:
@@ -52,7 +52,7 @@ if __name__ == "__main__":
             preset_path=engine_root() / "presets.yaml",
         ),
         user_dict=UserDictionary(),
-        engine_manifest=load_manifest(engine_root() / "engine_manifest.json"),
+        engine_manifest=load_manifest(engine_manifest_path()),
     )
     api_schema = json.dumps(app.openapi())
 
