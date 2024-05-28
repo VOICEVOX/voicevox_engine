d59a2ecbd5013ed7a2df24f48e66d7d683228cf2
tarepan <tarepan5884@gmail.com>
2024-05-28 12:29:33 +0900

    整理: タグ付与をルーターに集約 (#1226)
    refactor: タグ付与をルーターに集約

--- voicevox_engine/app/routers/speaker.py
+++ voicevox_engine/app/routers/speaker.py
@@ -24,20 +24,15 @@ def generate_speaker_router(
     root_dir: Path,
 ) -> APIRouter:
     """話者情報 API Router を生成する"""
-    router = APIRouter()
+    router = APIRouter(tags=["その他"])
 
-    @router.get("/speakers", tags=["その他"])
-    def speakers(
-        core_version: str | None = None,
-    ) -> list[Speaker]:
+    @router.get("/speakers")
+    def speakers(core_version: str | None = None) -> list[Speaker]:
         speakers = metas_store.load_combined_metas(core_manager.get_core(core_version))
         return filter_speakers_and_styles(speakers, "speaker")
 
-    @router.get("/speaker_info", tags=["その他"])
-    def speaker_info(
-        speaker_uuid: str,
-        core_version: str | None = None,
-    ) -> SpeakerInfo:
+    @router.get("/speaker_info")
+    def speaker_info(speaker_uuid: str, core_version: str | None = None) -> SpeakerInfo:
         """
         指定されたspeaker_uuidに関する情報をjson形式で返します。
         画像や音声はbase64エンコードされたものが返されます。
@@ -143,18 +138,13 @@ def generate_speaker_router(
         )
         return ret_data
 
-    @router.get("/singers", tags=["その他"])
-    def singers(
-        core_version: str | None = None,
-    ) -> list[Speaker]:
+    @router.get("/singers")
+    def singers(core_version: str | None = None) -> list[Speaker]:
         singers = metas_store.load_combined_metas(core_manager.get_core(core_version))
         return filter_speakers_and_styles(singers, "singer")
 
-    @router.get("/singer_info", tags=["その他"])
-    def singer_info(
-        speaker_uuid: str,
-        core_version: str | None = None,
-    ) -> SpeakerInfo:
+    @router.get("/singer_info")
+    def singer_info(speaker_uuid: str, core_version: str | None = None) -> SpeakerInfo:
         """
         指定されたspeaker_uuidに関する情報をjson形式で返します。
         画像や音声はbase64エンコードされたものが返されます。
@@ -165,7 +155,7 @@ def generate_speaker_router(
             core_version=core_version,
         )
 
-    @router.post("/initialize_speaker", status_code=204, tags=["その他"])
+    @router.post("/initialize_speaker", status_code=204)
     def initialize_speaker(
         style_id: Annotated[StyleId, Query(alias="speaker")],
         skip_reinit: Annotated[
@@ -183,7 +173,7 @@ def generate_speaker_router(
         core = core_manager.get_core(core_version)
         core.initialize_style_id_synthesis(style_id, skip_reinit=skip_reinit)
 
-    @router.get("/is_initialized_speaker", tags=["その他"])
+    @router.get("/is_initialized_speaker")
     def is_initialized_speaker(
         style_id: Annotated[StyleId, Query(alias="speaker")],
         core_version: str | None = None,
