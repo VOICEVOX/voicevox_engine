791960ca5e9b26799b65ac0d34ba8e1816a54cb5
tarepan <tarepan5884@gmail.com>
2024-06-02 16:23:42 +0000

    refactor: bash コマンドを簡略化

--- build_util/create_venv_and_generate_licenses.bash
+++ build_util/create_venv_and_generate_licenses.bash
@@ -20,7 +20,7 @@ fi
 
 pip install -r requirements.txt
 # requirements-dev.txt でバージョン指定されている pip-licenses をインストールする
-pip install "$(grep pip-licenses < requirements-dev.txt | cut -f 1 -d ';')"
+pip install "$(grep pip-licenses requirements-dev.txt | cut -f 1 -d ';')"
 python build_util/generate_licenses.py > "${OUTPUT_LICENSE_JSON_PATH}"
 
 deactivate
