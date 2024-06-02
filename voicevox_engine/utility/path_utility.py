24f3c76731d572b86bd58917a206426b582c826f
Hiroshiba <hihokaruta@gmail.com>
2024-06-03 05:08:32 +0900

    Update voicevox_engine/utility/path_utility.py

--- voicevox_engine/utility/path_utility.py
+++ voicevox_engine/utility/path_utility.py
@@ -29,7 +29,10 @@ def engine_manifest_path() -> Path:
 
 
 def _is_development() -> bool:
-    """動作環境が開発版（Pyinstallerでコンパイルされていない場合）であるか否かを返す。"""
+    """
+    動作環境が開発版であるか否かを返す。
+    Pyinstallerでコンパイルされていない場合は開発環境とする。
+    """
     # pyinstallerでビルドをした際はsys.frozenが設定される
     return False if getattr(sys, "frozen", False) else True
 
