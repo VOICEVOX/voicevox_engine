1ddd3143303a31beba289daf357284f358a8a8c1
Hiroshiba <hihokaruta@gmail.com>
2024-06-11 05:02:56 +0900

    Update voicevox_engine/app/application.py

--- voicevox_engine/app/application.py
+++ voicevox_engine/app/application.py
@@ -53,7 +53,7 @@ def generate_app(
         title=engine_manifest.name,
         description=f"{engine_manifest.brand_name} の音声合成エンジンです。",
         version=__version__,
-        separate_input_output_schemas=False,
+        separate_input_output_schemas=False,  # Pydantic V1 のときのスキーマに合わせるため
     )
     app = configure_middlewares(app, cors_policy_mode, allow_origin)
     app = configure_global_exception_handlers(app)
