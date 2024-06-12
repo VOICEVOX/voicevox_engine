b552286266aa85859eca82105b9b02600f2fa927
tarepan <tarepan5884@gmail.com>
2024-06-12 01:12:19 +0000

    refactor: `EngineManifestJson` の属性へ直接アクセス

--- voicevox_engine/engine_manifest.py
+++ voicevox_engine/engine_manifest.py
@@ -137,35 +137,31 @@ def load_manifest(manifest_path: Path) -> EngineManifest:
     root_dir = manifest_path.parent
     manifest = EngineManifestJson.model_validate_json(
         manifest_path.read_bytes()
-    ).model_dump()
+    )
     return EngineManifest(
-        manifest_version=manifest["manifest_version"],
-        name=manifest["name"],
-        brand_name=manifest["brand_name"],
-        uuid=manifest["uuid"],
-        url=manifest["url"],
-        default_sampling_rate=manifest["default_sampling_rate"],
-        frame_rate=manifest["frame_rate"],
-        icon=b64encode((root_dir / manifest["icon"]).read_bytes()).decode("utf-8"),
-        terms_of_service=(root_dir / manifest["terms_of_service"]).read_text("utf-8"),
+        manifest_version=manifest.manifest_version,
+        name=manifest.name,
+        brand_name=manifest.brand_name,
+        uuid=manifest.uuid,
+        url=manifest.url,
+        default_sampling_rate=manifest.default_sampling_rate,
+        frame_rate=manifest.frame_rate,
+        icon=b64encode((root_dir / manifest.icon).read_bytes()).decode("utf-8"),
+        terms_of_service=(root_dir / manifest.terms_of_service).read_text("utf-8"),
         update_infos=[
             UpdateInfo(**update_info)
             for update_info in json.loads(
-                (root_dir / manifest["update_infos"]).read_text("utf-8")
+                (root_dir / manifest.update_infos).read_text("utf-8")
             )
         ],
-        # supported_vvlib_manifest_versionを持たないengine_manifestのために
-        # キーが存在しない場合はNoneを返すgetを使う
-        supported_vvlib_manifest_version=manifest.get(
-            "supported_vvlib_manifest_version"
-        ),
+        supported_vvlib_manifest_version=None,
         dependency_licenses=[
             LicenseInfo(**license_info)
             for license_info in json.loads(
-                (root_dir / manifest["dependency_licenses"]).read_text("utf-8")
+                (root_dir / manifest.dependency_licenses).read_text("utf-8")
             )
         ],
         supported_features={
-            key: item["value"] for key, item in manifest["supported_features"].items()
+            key: item["value"] for key, item in manifest.supported_features.model_dump().items()
         },
     )
