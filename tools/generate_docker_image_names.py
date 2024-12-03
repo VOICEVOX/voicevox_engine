06ea55fabe42d85c7b1e49b63983b92ec9fd1e1d
aoirint <aoirint@gmail.com>
2024-12-03 09:46:33 +0900

    update usage

--- tools/generate_docker_image_names.py
+++ tools/generate_docker_image_names.py
@@ -3,18 +3,27 @@
 # バージョン文字列付きのDockerイメージ名を改行区切りで標準出力に出力する
 #
 # 例1
-# $ ./tools/generate_docker_image_names.bash "voicevox/voicevox_engine" "0.22.0" ",cpu,cpu-ubuntu22.04"
+# $ python3 ./tools/generate_docker_image_names.py \
+#   --repository "voicevox/voicevox_engine" \
+#   --version "0.22.0" \
+#   --prefix ",cpu,cpu-ubuntu22.04"
 # voicevox/voicevox_engine:0.22.0
 # voicevox/voicevox_engine:cpu-0.22.0
 # voicevox/voicevox_engine:cpu-ubuntu22.04-0.22.0
 #
 # 例2
-# $ ./tools/generate_docker_image_names.bash "voicevox/voicevox_engine" "latest" "nvidia,nvidia-ubuntu22.04"
+# $ python3 ./tools/generate_docker_image_names.py \
+#   --repository "voicevox/voicevox_engine" \
+#   --version "latest" \
+#   --prefix "nvidia,nvidia-ubuntu22.04"
 # voicevox/voicevox_engine:nvidia-latest
 # voicevox/voicevox_engine:nvidia-ubuntu22.04-latest
 #
 # 例3
-# $ ./tools/generate_docker_image_names.bash "voicevox/voicevox_engine" "latest" ""
+# $ python3 ./tools/generate_docker_image_names.py \
+#   --repository "voicevox/voicevox_engine" \
+#   --version "latest" \
+#   --prefix ""
 # voicevox/voicevox_engine:latest
 """
 
