1273be9bec432f88da9c6429ca7331a6a73e1b6f
takana-v <44311840+takana-v@users.noreply.github.com>
2025-03-12 19:54:32 +0900

    test: `extract_fullcontext_with_e2k`のe2eテストを追加

--- test/e2e/single_api/tts_pipeline/test_audio_query_from_preset.py
+++ test/e2e/single_api/tts_pipeline/test_audio_query_from_preset.py
@@ -37,6 +37,36 @@ def test_post_audio_query_from_preset_200(
     assert snapshot_json == round_floats(response.json(), 2)
 
 
+def test_post_audio_query_from_preset_enable_e2k_200(
+    client: TestClient, snapshot_json: SnapshotAssertion
+) -> None:
+    # Setup
+    # NOTE: 事前準備用のプリセット API が壊れた場合、このテストが偽陽性で failed になる可能性がある
+    preset = {
+        "id": 8888,
+        "name": "test_preset",
+        "speaker_uuid": "123-456-789-234",
+        "style_id": 9999,
+        "speedScale": 1.1,
+        "pitchScale": 0.9,
+        "intonationScale": 1.2,
+        "volumeScale": 1.3,
+        "prePhonemeLength": 20,
+        "postPhonemeLength": 5,
+        "pauseLength": 15,
+        "pauseLengthScale": 1.4,
+    }
+    client.post("/add_preset", params={}, json=preset)
+
+    # Test
+    response = client.post(
+        "/audio_query_from_preset",
+        params={"text": "Voivo", "preset_id": 8888, "enable_e2k": True},
+    )
+    assert response.status_code == 200
+    assert snapshot_json == round_floats(response.json(), 2)
+
+
 def test_post_audio_query_from_preset_422(
     client: TestClient, snapshot_json: SnapshotAssertion
 ) -> None:
