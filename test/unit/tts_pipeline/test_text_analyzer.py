2a90ead96ff6b6251b7ee57c8a79e32d5733cde7
tarepan <tarepan5884@gmail.com>
2025-03-21 13:35:52 +0900

    fix: テスト時のエラーチェック範囲を限局
    Co-authored-by: Hiroshiba <hihokaruta@gmail.com>

--- test/unit/tts_pipeline/test_text_analyzer.py
+++ test/unit/tts_pipeline/test_text_analyzer.py
@@ -374,8 +374,8 @@ def test_label_non_ojt_phoneme() -> None:
     """`Label` は OpenJTalk で想定されない音素をパース失敗する"""
     non_ojt_phoneme = "G"
     non_ojt_feature = f".^.-{non_ojt_phoneme}+.=./A:.+2+./B:.-._./C:._.+./D:.+._./E:._.!._.-./F:2_1#0_.@1_.|._./G:._.%._._./H:._./I:.-.@1+.&.-.|.+./J:._./K:.+.-."  # noqa: B950
+    unknown_label = Label.from_feature(non_ojt_feature)
     with pytest.raises(NonOjtPhonemeError):
-        unknown_label = Label.from_feature(non_ojt_feature)
         unknown_label.phoneme
 
 
