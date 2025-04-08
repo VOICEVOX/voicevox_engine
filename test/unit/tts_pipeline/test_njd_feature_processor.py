e383a5a74d859a7aa05a3ac8628ac86a82c4439d
takana-v <44311840+takana-v@users.noreply.github.com>
2025-04-08 23:47:46 +0900

    refactor: テスト用のfeature作成関数を追加して行数を削減

--- test/unit/tts_pipeline/test_njd_feature_processor.py
+++ test/unit/tts_pipeline/test_njd_feature_processor.py
@@ -1,64 +1,57 @@
+import re
+
 from voicevox_engine.tts_pipeline.njd_feature_processor import (
     NjdFeature,
     _remove_full_width_space,
 )
 
 
+def _gen_noun_feature(string: str) -> NjdFeature:
+    return NjdFeature(
+        string=string,
+        pos="名詞",
+        pos_group1="一般",
+        pos_group2="*",
+        pos_group3="*",
+        ctype="*",
+        cform="*",
+        orig=string,
+        read="ダミー",
+        pron="ダミー",
+        acc=0,
+        mora_size=3,
+        chain_rule="*",
+        chain_flag=0,
+    )
+
+
+def _gen_pau_space_feature() -> NjdFeature:
+    return NjdFeature(
+        string="　",
+        pos="記号",
+        pos_group1="空白",
+        pos_group2="*",
+        pos_group3="*",
+        ctype="*",
+        cform="*",
+        orig="　",
+        read="、",
+        pron="、",
+        acc=0,
+        mora_size=0,
+        chain_rule="*",
+        chain_flag=0,
+    )
+
+
 def test_remove_full_width_space_normal() -> None:
     # Inputs
     input_features = [
-        NjdFeature(
-            string="　",
-            pos="記号",
-            pos_group1="空白",
-            pos_group2="*",
-            pos_group3="*",
-            ctype="*",
-            cform="*",
-            orig="　",
-            read="、",
-            pron="、",
-            acc=0,
-            mora_size=0,
-            chain_rule="*",
-            chain_flag=0,
-        ),
-        NjdFeature(
-            string="ボイボ",
-            pos="名詞",
-            pos_group1="一般",
-            pos_group2="*",
-            pos_group3="*",
-            ctype="*",
-            cform="*",
-            orig="ボイボ",
-            read="ボイボ",
-            pron="ボイボ",
-            acc=1,
-            mora_size=3,
-            chain_rule="*",
-            chain_flag=0,
-        ),
+        _gen_pau_space_feature(),
+        _gen_noun_feature("ボイボ"),
     ]
     # Expects
-    true_features = [
-        NjdFeature(
-            string="ボイボ",
-            pos="名詞",
-            pos_group1="一般",
-            pos_group2="*",
-            pos_group3="*",
-            ctype="*",
-            cform="*",
-            orig="ボイボ",
-            read="ボイボ",
-            pron="ボイボ",
-            acc=1,
-            mora_size=3,
-            chain_rule="*",
-            chain_flag=0,
-        )
-    ]
+    true_features = [_gen_noun_feature("ボイボ")]
     # Outputs
     result = _remove_full_width_space(input_features)
     # Tests
@@ -67,24 +60,7 @@ def test_remove_full_width_space_normal() -> None:
 
 def test_remove_full_width_space_non_matching() -> None:
     # Inputs
-    input_features = [
-        NjdFeature(
-            string="ボイボ",
-            pos="名詞",
-            pos_group1="一般",
-            pos_group2="*",
-            pos_group3="*",
-            ctype="*",
-            cform="*",
-            orig="ボイボ",
-            read="ボイボ",
-            pron="ボイボ",
-            acc=1,
-            mora_size=3,
-            chain_rule="*",
-            chain_flag=0,
-        )
-    ]
+    input_features = [_gen_noun_feature("ボイボ")]
     # Outputs
     result = _remove_full_width_space(input_features)
     # Tests
