db809bc0a9e451162c2623b27c7e2205d6d584b6
tarepan <tarepan5884@gmail.com>
2025-04-19 15:26:41 +0000

    refactor function docstring and add TODO

--- voicevox_engine/user_dict/model.py
+++ voicevox_engine/user_dict/model.py
@@ -113,8 +113,9 @@ class UserDictWord(BaseModel):
 
     @model_validator(mode="after")
     def check_mora_count_and_accent_type(self) -> Self:
-        """モーラ数を補完し、アクセント型をチェックする。"""
-        # モーラ数を補完する
+        """モーラ数が None であれば計算し、アクセント型を検証する。"""
+        # TODO: 2つの機能を単一関数に入れるのが正しいか検討
+        # モーラ数を計算し代入する
         if self.mora_count is None:
             rule_others = (
                 "[イ][ェ]|[ヴ][ャュョ]|[ウクグトド][ゥ]|[テデ][ィェャュョ]|[クグ][ヮ]"
@@ -128,7 +129,7 @@ class UserDictWord(BaseModel):
                     self.pronunciation,
                 )
             )
-        # アクセント型をチェックする
+        # アクセント型を検証する
         if not 0 <= self.accent_type <= self.mora_count:
             raise ValueError(
                 f"誤ったアクセント型です({self.accent_type})。 expect: 0 <= accent_type <= {self.mora_count}"
