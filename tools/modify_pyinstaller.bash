6916e5e1349eaa3738f3a0dfd42ba13688cf4f72
nanae <desperado.kazune@gmail.com>
2025-04-25 03:45:20 +0900

    chore: uvへ移行する(Dockerコンテナ以外) (#1616)
    Co-authored-by: Hiroshiba <hihokaruta@gmail.com>

--- tools/modify_pyinstaller.bash
+++ tools/modify_pyinstaller.bash
@@ -8,7 +8,7 @@
 
 set -eux
 
-pyinstaller_version=$(pyinstaller -v)
+pyinstaller_version=$(uv run pyinstaller -v)
 tempdir=$(mktemp -dt modify_pyinstaller.XXXXXXXX)
 trap 'rm -rf "$tempdir"' EXIT
 git clone https://github.com/pyinstaller/pyinstaller.git "$tempdir" -b "v$pyinstaller_version" --depth 1
@@ -23,5 +23,5 @@ __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
 __declspec(dllexport) DWORD AmdPowerXpressRequestHighPerformance = 0x00000001;
 #endif
 EOF
-(cd "$tempdir/bootloader" && python ./waf all --msvc_targets="x64")
-pip install -U "$tempdir"
+(cd "$tempdir/bootloader" && uv run ./waf all --msvc_targets="x64")
+uv pip install -U "$tempdir" # TODO: 可能であればuv addでやる
