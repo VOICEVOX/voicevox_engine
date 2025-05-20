b56b3784287bb842d5303e9cb1a42c77741ea01e
tarepan <tarepan5884@gmail.com>
2025-05-20 04:31:02 +0000

    fix public/private name

--- voicevox_engine/utility/text_utility.py
+++ voicevox_engine/utility/text_utility.py
@@ -2,18 +2,18 @@
 
 from typing import Final
 
-HANKAKU_CHARACTERS: Final = "".join(chr(0x21 + i) for i in range(94))
-ZENKAKU_CHARACTERS: Final = "".join(chr(0xFF01 + i) for i in range(94))
+_HANKAKU_CHARS: Final = "".join(chr(0x21 + i) for i in range(94))
+_ZENKAKU_CHARS: Final = "".join(chr(0xFF01 + i) for i in range(94))
 
-HANKAKU_TO_ZENKAKU_TABLE: Final = str.maketrans(HANKAKU_CHARACTERS, ZENKAKU_CHARACTERS)
-ZENKAKU_TO_HANKAKU_TABLE: Final = str.maketrans(ZENKAKU_CHARACTERS, HANKAKU_CHARACTERS)
+_HANKAKU_TO_ZENKAKU_TABLE: Final = str.maketrans(_HANKAKU_CHARS, _ZENKAKU_CHARS)
+_ZENKAKU_TO_HANKAKU_TABLE: Final = str.maketrans(_ZENKAKU_CHARS, _HANKAKU_CHARS)
 
 
 def replace_hankaku_alphabets_with_zenkaku(string: str) -> str:
     """文字列に含まれる半角アルファベットを全角アルファベットで置き換える。"""
-    return string.translate(HANKAKU_TO_ZENKAKU_TABLE)
+    return string.translate(_HANKAKU_TO_ZENKAKU_TABLE)
 
 
 def replace_zenkaku_alphabets_with_hankaku(string: str) -> str:
     """文字列に含まれる全角アルファベットを半角アルファベットで置き換える。"""
-    return string.translate(ZENKAKU_TO_HANKAKU_TABLE)
+    return string.translate(_ZENKAKU_TO_HANKAKU_TABLE)
