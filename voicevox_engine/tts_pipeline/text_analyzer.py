7fb1129d33e0a37bf19562580d8ca2e5bdae83ab
tarepan <tarepan5884@gmail.com>
2025-06-22 16:53:16 +0000

    refactor vowel acquisition

--- voicevox_engine/tts_pipeline/text_analyzer.py
+++ voicevox_engine/tts_pipeline/text_analyzer.py
@@ -194,9 +194,7 @@ class _AccentPhraseLabel:
     def from_labels(cls, labels: list[_Label]) -> Self:
         """ラベル系列を区切ってアクセント句ラベルを生成する。"""
         moras: list[Mora] = []
-        accent: int | None = None
-        is_interrogative: bool | None = None
-
+        vowel: _Label | None = None
         for mora_index, _mora_labels in groupby(labels, lambda label: label.mora_index):
             mora_labels = list(_mora_labels)
 
@@ -215,28 +213,19 @@ class _AccentPhraseLabel:
                     raise ValueError(mora_labels)
             moras.append(_generate_mora(consonant=consonant, vowel=vowel))
 
-            if len(moras) == 1:
-                if vowel.accent_position is None:
-                    msg = "アクセント位置が指定されていません。"
-                    raise RuntimeError(msg)
-                accent = vowel.accent_position
+        if vowel is None:
+            msg = "母音が取得できません。"
+            raise RuntimeError(msg)
 
-            # NOTE: 末尾モーラの疑問文フラグがアクセント句の疑問文フラグになる
-            is_interrogative = vowel.is_interrogative
-
-        if accent is None:
+        if vowel.accent_position is None:
             msg = "アクセント位置が指定されていません。"
             raise RuntimeError(msg)
+        accent = vowel.accent_position
         # アクセント位置の値がアクセント句内のモーラ数を超える場合はクリップ（ワークアラウンド、VOICEVOX/voicevox_engine#55 を参照）
         accent = accent if accent <= len(moras) else len(moras)
 
-        if is_interrogative is None:
-            msg = "疑問形フラグが指定されていません。"
-            raise RuntimeError(msg)
-
-        # アクセント句ラベルを生成する
         accent_phrase = cls(
-            moras=moras, accent=accent, is_interrogative=is_interrogative
+            moras=moras, accent=accent, is_interrogative=vowel.is_interrogative
         )
 
         return accent_phrase
